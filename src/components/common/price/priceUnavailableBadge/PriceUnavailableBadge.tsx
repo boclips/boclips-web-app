28ac64a0b37836a0288eb3a07d00f07157415bd5
Mima Farley-Rose <mima@boclips.com>
2021-09-08 17:05:32 +0100

    Ui tweak
    -Change the font weight on the price-unavailable badge
    
    [#179353707]

--- src/components/common/price/priceUnavailableBadge/PriceUnavailableBadge.tsx
+++ src/components/common/price/priceUnavailableBadge/PriceUnavailableBadge.tsx
@@ -3,7 +3,7 @@ import { Tooltip } from '@boclips-ui/tooltip';
 
 export const PriceUnavailableBadge = () => (
   <Tooltip text="For pricing information contact your local Pearson R&P team">
-    <span className="border-2 border-gray-400 rounded text-gray-800 px-2">
+    <span className="border-2 border-gray-400 font-normal	 rounded text-gray-800 px-2">
       Price unavailable
     </span>
   </Tooltip>
