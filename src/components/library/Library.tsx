c88463409b22459b6a2fcd191e813f55059e2b69
Harsha Chaudhary <harsha@boclips.com>
2022-01-25 16:04:01 +0530

    Fixes style of library page title
    [#180839275]

--- src/components/library/Library.tsx
+++ src/components/library/Library.tsx
@@ -8,7 +8,7 @@ const Library = (): ReactElement => {
   return (
     <>
       <div className="col-start-2 col-end-26 grid-row-start-2 grid-row-end-2 flex flex-row">
-        <div className="font-bold text-2xl">Your Library</div>
+        <div className={`${s.title} text-2xl`}>Your Library</div>
       </div>
 
       <div className={s.playlistsWrapper}>
