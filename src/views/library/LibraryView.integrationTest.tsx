e8285c1160d823f8e37da28edd057255f3f6f2b2
Eva Varga <eva@boclips.com>
2022-01-31 17:11:27 +0000

    Restructure LibraryView integration tests
    - was becoming quite messy
    - ignore false positive test while apiClient is not released
    - remove beforeEach and set up everything in each test individually -
    caused some multiple rendering issues
    
    [#180839676]

--- src/views/library/LibraryView.integrationTest.tsx
+++ src/views/library/LibraryView.integrationTest.tsx
@@ -1,7 +1,7 @@
 import {
   fireEvent,
   render,
-  screen,
+  RenderResult,
   waitFor,
   waitForElementToBeRemoved,
 } from '@testing-library/react';
@@ -16,216 +16,194 @@ import userEvent from '@testing-library/user-event';
 import { CollectionFactory } from 'src/testSupport/CollectionFactory';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-describe('LibraryView', () => {
-  const renderLibraryView = (client: BoclipsClient) =>
-    render(
+const setPlaylistsFeature = (client: FakeBoclipsClient, enabled: boolean) =>
+  client.users.insertCurrentUser(
+    UserFactory.sample({ features: { BO_WEB_APP_ENABLE_PLAYLISTS: enabled } }),
+  );
+
+const renderLibraryView = (client: BoclipsClient) =>
+  render(
+    <QueryClientProvider client={new QueryClient()}>
       <MemoryRouter initialEntries={['/library']}>
         <App apiClient={client} boclipsSecurity={stubBoclipsSecurity} />
-      </MemoryRouter>,
-    );
+      </MemoryRouter>
+    </QueryClientProvider>,
+  );
 
-  describe('when playlists feature is enabled', () => {
-    const client = new FakeBoclipsClient();
-    beforeEach(() => {
-      client.users.insertCurrentUser(
-        UserFactory.sample({ features: { BO_WEB_APP_ENABLE_PLAYLISTS: true } }),
-      );
-      renderLibraryView(client);
+describe('LibraryView', () => {
+  describe('Feature disabled', () => {
+    it('shows a blank page if feature flag not set', async () => {
+      const client = new FakeBoclipsClient();
+      setPlaylistsFeature(client, false);
+      const wrapper = renderLibraryView(client);
+      expect(await wrapper.queryByText('Your Library')).not.toBeInTheDocument();
+    });
+  });
+
+  describe('Feature enabled', () => {
+    it('loads the title for library page', async () => {
+      const client = new FakeBoclipsClient();
+      setPlaylistsFeature(client, true);
+      const wrapper = renderLibraryView(client);
+      expect(await wrapper.findByText('Your Library')).toBeVisible();
     });
 
-    it('loads the title for library page', async () => {
-      expect(await screen.findByText('Your Library')).toBeVisible();
+    // TODO() - this was a false positive, need to update the FakeApiClient for this to work properly
+    xit('renders playlists created by the user', async () => {
+      const client = new FakeBoclipsClient();
+      setPlaylistsFeature(client, true);
+
+      const playlists = [
+        CollectionFactory.sample({ title: 'Playlist 1' }),
+        CollectionFactory.sample({ title: 'Playlist 2' }),
+      ];
+
+      playlists.forEach((it) => client.collections.addToFake(it));
+
+      const wrapper = renderLibraryView(client);
+
+      expect(await wrapper.findByText('Playlist 1')).toBeVisible();
+      expect(await wrapper.findByText('Playlist 2')).toBeVisible();
     });
 
     describe('Creating playlists', () => {
       it('shows Create new playlist button', async () => {
+        const client = new FakeBoclipsClient();
+        setPlaylistsFeature(client, true);
+        const wrapper = renderLibraryView(client);
+
         expect(
-          await screen.findByRole('button', { name: 'Create new playlist' }),
+          await wrapper.findByRole('button', { name: 'Create new playlist' }),
         ).toBeVisible();
       });
 
       it('opens a modal when clicking the Create new playlist button', async () => {
-        const createPlaylistButton = await screen.findByRole('button', {
+        const client = new FakeBoclipsClient();
+        setPlaylistsFeature(client, true);
+        const wrapper = renderLibraryView(client);
+
+        const createPlaylistButton = await wrapper.findByRole('button', {
           name: 'Create new playlist',
         });
 
-        expect(screen.queryByLabelText('Create new playlist')).toBeNull();
+        expect(wrapper.queryByLabelText('Create new playlist')).toBeNull();
 
         fireEvent.click(createPlaylistButton);
 
-        expect(screen.getByLabelText('Create new playlist')).toBeVisible();
-        expect(screen.getByLabelText('*Playlist name')).toBeVisible();
-        expect(screen.getByPlaceholderText('Give it a name')).toBeVisible();
-        expect(screen.getByLabelText('Description')).toBeVisible();
-        expect(screen.getByPlaceholderText('Add description')).toBeVisible();
-        expect(screen.getByRole('button', { name: 'Cancel' })).toBeVisible();
+        expect(wrapper.getByLabelText('Create new playlist')).toBeVisible();
+        expect(wrapper.getByLabelText('*Playlist name')).toBeVisible();
+        expect(wrapper.getByPlaceholderText('Give it a name')).toBeVisible();
+        expect(wrapper.getByLabelText('Description')).toBeVisible();
+        expect(wrapper.getByPlaceholderText('Add description')).toBeVisible();
+        expect(wrapper.getByRole('button', { name: 'Cancel' })).toBeVisible();
         expect(
-          screen.getByRole('button', { name: 'Create new playlist' }),
+          wrapper.getByRole('button', { name: 'Create new playlist' }),
         ).toBeVisible();
       });
 
       it('can cancel modal', () => {
-        openPlaylistCreationModal();
+        const client = new FakeBoclipsClient();
+        setPlaylistsFeature(client, true);
+        const wrapper = renderLibraryView(client);
 
-        fireEvent.click(screen.getByRole('button', { name: 'Cancel' }));
+        openPlaylistCreationModal(wrapper);
 
-        const modal = screen.queryByLabelText('Create new playlist');
+        fireEvent.click(wrapper.getByRole('button', { name: 'Cancel' }));
+
+        const modal = wrapper.queryByLabelText('Create new playlist');
         expect(modal).not.toBeInTheDocument();
       });
 
       it('cannot create a playlist without a title', () => {
-        openPlaylistCreationModal();
+        const client = new FakeBoclipsClient();
+        setPlaylistsFeature(client, true);
+        const wrapper = renderLibraryView(client);
+
+        openPlaylistCreationModal(wrapper);
 
         fireEvent.click(
-          screen.getByRole('button', { name: 'Create playlist' }),
+          wrapper.getByRole('button', { name: 'Create playlist' }),
         );
-        const modal = screen.queryByLabelText('Create new playlist');
+        const modal = wrapper.queryByLabelText('Create new playlist');
         expect(modal).toBeVisible();
-        expect(screen.getByText('Playlist name is required')).toBeVisible();
+        expect(wrapper.getByText('Playlist name is required')).toBeVisible();
       });
 
       it('can create a playlist with title and description', async () => {
-        openPlaylistCreationModal();
+        const client = new FakeBoclipsClient();
+        setPlaylistsFeature(client, true);
+        const wrapper = renderLibraryView(client);
 
-        const titleInput = screen.getByLabelText('*Playlist name');
-        userEvent.type(titleInput, 'My new playlist');
-
-        const descriptionInput = screen.getByLabelText('Description');
-        userEvent.type(descriptionInput, 'Blabla new playlist');
-
-        fireEvent.click(
-          screen.getByRole('button', { name: 'Create playlist' }),
-        );
+        openPlaylistCreationModal(wrapper);
+        fillPlaylistName(wrapper, 'My new playlist');
+        fillPlaylistDescription(wrapper, 'Blabla new playlist');
+        confirmPlaylistCreationModal(wrapper);
 
         await waitForElementToBeRemoved(
-          screen.queryByLabelText('Create new playlist'),
+          wrapper.queryByLabelText('Create new playlist'),
         );
 
-        expect(await screen.findByText('My new playlist')).toBeVisible();
-        expect(await screen.findByText('Blabla new playlist')).toBeVisible();
+        expect(await wrapper.findByText('My new playlist')).toBeVisible();
+        expect(await wrapper.findByText('Blabla new playlist')).toBeVisible();
       });
 
-      const openPlaylistCreationModal = () =>
+      it('can display an error message on failed playlist creation', async () => {
+        const client = new FakeBoclipsClient();
+        setPlaylistsFeature(client, true);
+        const wrapper = renderLibraryView(client);
+        client.collections.setCreateCollectionErrorMessage('500 server error');
+
+        openPlaylistCreationModal(wrapper);
+        fillPlaylistName(wrapper, 'My new playlist');
+        confirmPlaylistCreationModal(wrapper);
+
+        expect(
+          await wrapper.findByText('Error: Failed to create new playlist'),
+        ).toBeVisible();
+        expect(
+          await wrapper.findByText('Please refresh the page and try again'),
+        ).toBeVisible();
+      });
+
+      it('display spinner in confirm button after creating playlist', async () => {
+        const client = new FakeBoclipsClient();
+        setPlaylistsFeature(client, true);
+        const wrapper = renderLibraryView(client);
+
+        openPlaylistCreationModal(wrapper);
+        fillPlaylistName(wrapper, 'My new playlist');
+        confirmPlaylistCreationModal(wrapper);
+
         fireEvent.click(
-          screen.getByRole('button', { name: 'Create new playlist' }),
+          wrapper.getByRole('button', { name: 'Create playlist' }),
+        );
+
+        await waitFor(() => {
+          expect(wrapper.getByTestId('spinner')).toBeInTheDocument();
+        });
+      });
+
+      const openPlaylistCreationModal = (wrapper: RenderResult) =>
+        fireEvent.click(
+          wrapper.getByRole('button', { name: 'Create new playlist' }),
+        );
+
+      const fillPlaylistField = (
+        wrapper: RenderResult,
+        label: string,
+        value: string,
+      ) => userEvent.type(wrapper.getByLabelText(label), value);
+
+      const fillPlaylistName = (wrapper: RenderResult, value: string) =>
+        fillPlaylistField(wrapper, '*Playlist name', value);
+
+      const fillPlaylistDescription = (wrapper: RenderResult, value: string) =>
+        fillPlaylistField(wrapper, 'Description', value);
+
+      const confirmPlaylistCreationModal = (wrapper: RenderResult) =>
+        fireEvent.click(
+          wrapper.getByRole('button', { name: 'Create playlist' }),
         );
     });
-  });
-
-  describe('when playlists feature is disabled', () => {
-    const client = new FakeBoclipsClient();
-    beforeEach(() => {
-      client.users.insertCurrentUser(
-        UserFactory.sample({
-          features: { BO_WEB_APP_ENABLE_PLAYLISTS: false },
-        }),
-      );
-
-      renderLibraryView(client);
-    });
-
-    it('shows a blank page', async () => {
-      expect(await screen.queryByText('Your Library')).not.toBeInTheDocument();
-    });
-  });
-
-  const playlists = [
-    CollectionFactory.sample({
-      title: 'box',
-    }),
-    CollectionFactory.sample({
-      title: 'print',
-    }),
-    CollectionFactory.sample({
-      title: 'scorn',
-    }),
-    CollectionFactory.sample({
-      title: 'sing',
-    }),
-    CollectionFactory.sample({
-      title: 'group',
-    }),
-    CollectionFactory.sample({
-      title: 'kneel',
-    }),
-  ];
-
-  it('Displays tiles for retrieved playlists', async () => {
-    const fakeClient = new FakeBoclipsClient();
-    const queryClient = new QueryClient();
-
-    playlists.forEach((playlist) => {
-      fakeClient.collections.addToFake(playlist);
-    });
-
-    const wrapper = render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter initialEntries={['/library']}>
-          <App apiClient={fakeClient} boclipsSecurity={stubBoclipsSecurity} />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
-
-    playlists.map(async (it) => {
-      expect(await wrapper.findByText(it.title)).toBeInTheDocument();
-    });
-  });
-});
-
-it('can display an error message on failed playlist creation', async () => {
-  const client = new FakeBoclipsClient();
-  client.users.insertCurrentUser(
-    UserFactory.sample({ features: { BO_WEB_APP_ENABLE_PLAYLISTS: true } }),
-  );
-
-  const wrapper = render(
-    <MemoryRouter initialEntries={['/library']}>
-      <App apiClient={client} boclipsSecurity={stubBoclipsSecurity} />
-    </MemoryRouter>,
-  );
-  expect(
-    await wrapper.findByRole('button', { name: 'Create new playlist' }),
-  ).toBeVisible();
-
-  fireEvent.click(wrapper.getByRole('button', { name: 'Create new playlist' }));
-
-  const titleInput = wrapper.getByLabelText('*Playlist name');
-  userEvent.type(titleInput, 'My new playlist');
-
-  client.collections.setCreateCollectionErrorMessage('500 server error');
-
-  fireEvent.click(wrapper.getByRole('button', { name: 'Create playlist' }));
-
-  expect(
-    await wrapper.findByText('Error: Failed to create new playlist'),
-  ).toBeVisible();
-  expect(
-    await wrapper.findByText('Please refresh the page and try again'),
-  ).toBeVisible();
-});
-
-it('display spinner in confirm button after creating playlist', async () => {
-  const client = new FakeBoclipsClient();
-  client.users.insertCurrentUser(
-    UserFactory.sample({ features: { BO_WEB_APP_ENABLE_PLAYLISTS: true } }),
-  );
-
-  const wrapper = render(
-    <MemoryRouter initialEntries={['/library']}>
-      <App apiClient={client} boclipsSecurity={stubBoclipsSecurity} />
-    </MemoryRouter>,
-  );
-  expect(
-    await wrapper.findByRole('button', { name: 'Create new playlist' }),
-  ).toBeVisible();
-
-  fireEvent.click(wrapper.getByRole('button', { name: 'Create new playlist' }));
-
-  const titleInput = wrapper.getByLabelText('*Playlist name');
-  userEvent.type(titleInput, 'My new playlist');
-  fireEvent.click(wrapper.getByRole('button', { name: 'Create playlist' }));
-
-  await waitFor(() => {
-    expect(wrapper.getByTestId('spinner')).toBeInTheDocument();
   });
 });
