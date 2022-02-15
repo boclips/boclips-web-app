cc0044feb5c56a3e51f332ff9d66e07a8c77dd57
Eva Varga <eva@boclips.com>
2022-02-15 16:51:40 +0000

    Make bodal footer sticky in mobile view
    + don't show border outline on secondary button
    
    [#180839314]

--- src/components/common/overlay/Overlay.tsx
+++ src/components/common/overlay/Overlay.tsx
@@ -8,13 +8,15 @@ interface Props {
 export const Overlay: React.FC<Props> = ({ header, footer, children }) => {
   return (
     <div className={s.overlay} data-qa="overlay">
-      {header && (
-        <div className={s.overlayHeader}>
-          <span />
-          {header}
-        </div>
-      )}
-      <div>{children}</div>
+      <div className={s.overlayContent}>
+        {header && (
+          <div className={s.overlayHeader}>
+            <span />
+            {header}
+          </div>
+        )}
+        <div>{children}</div>
+      </div>
       {footer && <div className={s.overlayFooter}>{footer}</div>}
     </div>
   );
