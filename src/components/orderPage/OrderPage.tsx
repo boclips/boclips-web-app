15fd73389be0c63f7093be26dcc26d9b07f3d97f
Matt Jones <matt@boclips.com>
2022-02-16 11:38:31 +0000

    Add main landmark to order view
    [#181250530]

--- src/components/orderPage/OrderPage.tsx
+++ src/components/orderPage/OrderPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Order } from 'boclips-api-client/dist/sub-clients/orders/model/Order';
 import { OrderSummary } from 'src/components/orderPage/OrderSummary';
 import { OrderVideoList } from 'src/components/orderPage/OrderVideoList';
+import { Main } from '../layout/Main';
 
 interface Props {
   order: Order;
@@ -11,7 +12,9 @@ export const OrderPage = ({ order }: Props) => {
   return (
     <>
       <OrderSummary order={order} />
-      <OrderVideoList order={order} />
+      <Main>
+        <OrderVideoList order={order} />
+      </Main>
     </>
   );
 };
