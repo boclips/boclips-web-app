fb91ac697d6bc0a61f70c93170b4a9fbf95d0ac8
Pawel Tylczak <pawel@boclips.com>
2022-02-24 18:10:08 +0100

    Display additional services message
    - the message depends on the user's organisation
    - if this is a Pearson user, display 'free of charge'
    - if this is a non-Pearson user, do not display 'free of charge'
    
    [#181312204]
    
    Signed-off-by: Rafal Lewandowski <rafal@boclips.com>

--- src/components/videoPage/VideoAdditionalServices.tsx
+++ src/components/videoPage/VideoAdditionalServices.tsx
@@ -1,18 +1,27 @@
 import InfoIcon from 'resources/icons/info.svg';
 import React from 'react';
+import { useGetUserQuery } from 'src/hooks/api/userQuery';
+import { Constants } from 'src/AppConstants';
 
 export const VideoAdditionalServices = () => {
+  const { data: user, isLoading: isLoadingUser } = useGetUserQuery();
+
+  const userIsPearson =
+    user?.organisation?.id === Constants.PEARSON_ORGANISATION_ID;
+
+  const message = userIsPearson
+    ? 'Captions, transcripts, video trimming, and other editing requests are ' +
+      'available upon request from your shopping cart. All additional services are available free of charge.'
+    : 'Captions, transcripts, video trimming, and other editing requests are ' +
+      'available upon request from your shopping cart.';
+
   return (
     <div className="mt-4 bg-blue-100 border-blue-400 border-2 p-6 rounded text-gray-800">
       <div className="flex flex-row font-medium text-base items-center mb-2 text-gray-900">
         <InfoIcon />
         <div className="ml-2">Additional services</div>
       </div>
-      <div className="text-sm">
-        Captions, transcripts, video trimming, and other editing requests are
-        available upon request from your shopping cart. All additional services
-        are available free of charge.
-      </div>
+      {!isLoadingUser && <div className="text-sm">{message}</div>}
     </div>
   );
 };
