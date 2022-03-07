d8d05f8bc91da8aa6c6d826ccc521bddc0dd6dda
Remigiusz Oleszczuk <remy@boclips.com>
2022-03-07 09:52:52 +0100

    Update colours for mobile homepage
    [#181325593]

--- src/components/disciplinesWidget/DisciplineOverlayMenu.tsx
+++ src/components/disciplinesWidget/DisciplineOverlayMenu.tsx
@@ -38,6 +38,7 @@ const DisciplineOverlayMenu = ({ onClick, selectedDiscipline }: Props) => {
           return (
             <Link
               key={subject.name}
+              className=""
               to={{
                 pathname: '/videos',
                 search: `?subject=${subject.id}`,
