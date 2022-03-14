dcecadb8f7c2697d4426dd981fca9c0bc06e7b0c
Remigiusz Oleszczuk <remy@boclips.com>
2022-03-14 12:36:43 +0100

    Use jest.mocked instead of jest.mock in player token test

--- src/components/videoCard/VideoPlayer.test.tsx
+++ src/components/videoCard/VideoPlayer.test.tsx
@@ -3,9 +3,12 @@ import { VideoPlayer } from 'src/components/videoCard/VideoPlayer';
 import { render } from '@testing-library/react';
 import { VideoFactory } from 'boclips-api-client/dist/test-support/VideosFactory';
 import { PlayerFactory } from 'boclips-player';
-import { mocked } from 'ts-jest/utils';
 import { stubBoclipsSecurity } from 'src/testSupport/StubBoclipsSecurity';
 import { BoclipsSecurityProvider } from 'src/components/common/providers/BoclipsSecurityProvider';
+
+jest.mock('boclips-player');
+
+const mockedPlayer = jest.mocked(PlayerFactory, true);
 
 describe('VideoPlayer', () => {
   it('provides a token factory to the player that returns a valid token', async () => {
@@ -23,7 +26,7 @@ describe('VideoPlayer', () => {
 
     expect(PlayerFactory.get).toHaveBeenCalled();
 
-    const options = mocked(PlayerFactory.get).mock.calls[0][1];
+    const options = mockedPlayer.get.mock.calls[0][1];
     const providedTokenFactory = options.api.tokenFactory;
 
     await expect(providedTokenFactory()).resolves.toEqual('test-token');
