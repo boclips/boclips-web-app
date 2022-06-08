0dd8907df323e1216a70a4211c96aab0b1b6dff0
Alex Glen <alex@boclips.com>
2022-06-08 10:16:19 +0100

    Adjust search bubble styling
    [sc-296]

--- src/components/searchResults/Bubble.tsx
+++ src/components/searchResults/Bubble.tsx
@@ -1,6 +1,7 @@
 import c from 'classnames';
 import React from 'react';
 import { Facet } from 'boclips-api-client/dist/sub-clients/videos/model/VideoFacets';
+import { Typography } from '@boclips-ui/typography';
 
 interface Props {
   topic: Facet;
@@ -12,14 +13,15 @@ export const Bubble = ({ handleClick, selected, topic }: Props) => {
   return (
     <button
       type="button"
+      name={topic.name}
       data-qa="search-topic"
       onClick={() => handleClick()}
       className={c(
-        'rounded-full active:rounded-full focus:rounded-full inline-block py-1 px-4 mx-1 mt-1 border-2 border-gray-400 px-3 hover:cursor-pointer',
-        { 'bg-gray-600 border-gray-600': selected },
+        'rounded-full active:rounded-full focus:rounded-full inline-block border-solid py-1 px-4 mx-1 mt-1 border-2 border-gray-400 px-3',
+        { 'bg-gray-600 border-gray-600 text-white': selected },
       )}
     >
-      {topic.name}
+      <Typography.Body>{topic.name}</Typography.Body>
     </button>
   );
 };
