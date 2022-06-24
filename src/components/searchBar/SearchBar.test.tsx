b8d839978786d14502d421ed73985142339eb98b
Marcin Janik <marcin@boclips.com>
2022-06-24 17:14:59 +0200

    Send suggestionCompletion event with searchUrl
    ...when suggestion's been used to perform search
    
    [sc-375]

--- src/components/searchBar/SearchBar.test.tsx
+++ src/components/searchBar/SearchBar.test.tsx
@@ -7,6 +7,7 @@ import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { lastEvent } from 'src/testSupport/lastEvent';
 import { SearchQueryCompletionsSuggestedRequest } from 'boclips-api-client/dist/sub-clients/events/model/SearchQueryCompletionsSuggestedRequest';
+import { sleep } from 'src/testSupport/sleep';
 import { BoclipsClientProvider } from '../common/providers/BoclipsClientProvider';
 
 describe('SearchBar', () => {
@@ -51,138 +52,147 @@ describe('SearchBar', () => {
     );
   });
 
-  it('display search suggestions on search change if user has feature', async () => {
-    const fakeBoclipsClient = new FakeBoclipsClient();
-    fakeBoclipsClient.users.setCurrentUserFeatures({
-      BO_WEB_APP_SEARCH_SUGGESTIONS: true,
+  describe('search suggestions', () => {
+    let fakeBoclipsClient;
+    beforeEach(() => {
+      fakeBoclipsClient = new FakeBoclipsClient();
+      fakeBoclipsClient.users.setCurrentUserFeatures({
+        BO_WEB_APP_SEARCH_SUGGESTIONS: true,
+      });
+
+      fakeBoclipsClient.suggestions.populate({
+        channels: [],
+        subjects: [],
+        suggestionTerm: 'U',
+        phrases: ['U.S. Senate', 'U.S. Government'],
+      });
     });
 
-    const wrapper = render(
-      <BoclipsClientProvider client={fakeBoclipsClient}>
-        <Router history={createBrowserHistory()}>
-          <Search showIconOnly={false} />
-        </Router>
-      </BoclipsClientProvider>,
-    );
+    it('display search suggestions on search change if user has feature', async () => {
+      const wrapper = render(
+        <BoclipsClientProvider client={fakeBoclipsClient}>
+          <Router history={createBrowserHistory()}>
+            <Search showIconOnly={false} />
+          </Router>
+        </BoclipsClientProvider>,
+      );
 
-    fakeBoclipsClient.suggestions.populate({
-      channels: [],
-      subjects: [],
-      suggestionTerm: 'U.S. ',
-      phrases: ['U.S. Senate', 'U.S. Constitution'],
+      const searchInput = wrapper.getByPlaceholderText(
+        'Search for videos',
+      ) as HTMLInputElement;
+      fireEvent.change(searchInput, { target: { value: 'U' } });
+      fireEvent.focus(searchInput);
+
+      expect(await wrapper.findByText('.S. Government')).toBeInTheDocument();
+      expect(await wrapper.findByText('.S. Senate')).toBeInTheDocument();
     });
 
-    const searchInput = wrapper.getByPlaceholderText(
-      'Search for videos',
-    ) as HTMLInputElement;
-    fireEvent.change(searchInput, { target: { value: 'U.S.' } });
-    fireEvent.focus(searchInput);
+    it('display search suggestions on search change for minimum of 1 character in query', async () => {
+      const wrapper = render(
+        <BoclipsClientProvider client={fakeBoclipsClient}>
+          <Router history={createBrowserHistory()}>
+            <Search showIconOnly={false} />
+          </Router>
+        </BoclipsClientProvider>,
+      );
 
-    expect(wrapper.queryByText('Constitution')).toBeNull();
-    expect(await wrapper.findByText('Constitution')).toBeInTheDocument();
-    expect(await wrapper.findByText('Senate')).toBeInTheDocument();
-  });
+      const searchInput = wrapper.getByPlaceholderText(
+        'Search for videos',
+      ) as HTMLInputElement;
+      fireEvent.change(searchInput, { target: { value: 'U' } });
+      fireEvent.focus(searchInput);
 
-  it('display search suggestions on search change for minimum of 1 character in query', async () => {
-    const fakeBoclipsClient = new FakeBoclipsClient();
-    fakeBoclipsClient.users.setCurrentUserFeatures({
-      BO_WEB_APP_SEARCH_SUGGESTIONS: true,
+      expect(await wrapper.findByText('.S. Government')).toBeInTheDocument();
+      expect(await wrapper.findByText('.S. Senate')).toBeInTheDocument();
     });
 
-    const wrapper = render(
-      <BoclipsClientProvider client={fakeBoclipsClient}>
-        <Router history={createBrowserHistory()}>
-          <Search showIconOnly={false} />
-        </Router>
-      </BoclipsClientProvider>,
-    );
+    it(`does not display search auto-suggest (nor send events) if user does not have feature flag`, async () => {
+      fakeBoclipsClient.users.setCurrentUserFeatures({
+        BO_WEB_APP_SEARCH_SUGGESTIONS: false,
+      });
 
-    fakeBoclipsClient.suggestions.populate({
-      channels: [],
-      subjects: [],
-      suggestionTerm: 'U',
-      phrases: ['U.S. Senate', 'U.S. Government'],
+      const wrapper = render(
+        <BoclipsClientProvider client={fakeBoclipsClient}>
+          <Router history={createBrowserHistory()}>
+            <Search showIconOnly={false} />
+          </Router>
+        </BoclipsClientProvider>,
+      );
+
+      const searchInput = wrapper.getByPlaceholderText(
+        'Search for videos',
+      ) as HTMLInputElement;
+      fireEvent.change(searchInput, { target: { value: 'U' } });
+      fireEvent.focus(searchInput);
+
+      await sleep(500);
+
+      expect(searchInput.value).toEqual('U');
+      expect(wrapper.queryByText('.S. Constitution')).toBeNull();
+      expect(fakeBoclipsClient.events.getEvents()).toHaveLength(0);
     });
 
-    const searchInput = wrapper.getByPlaceholderText(
-      'Search for videos',
-    ) as HTMLInputElement;
-    fireEvent.change(searchInput, { target: { value: 'U' } });
-    fireEvent.focus(searchInput);
+    it('sends an event when auto-suggest displayed', async () => {
+      const wrapper = render(
+        <BoclipsClientProvider client={fakeBoclipsClient}>
+          <Router history={createBrowserHistory()}>
+            <Search showIconOnly={false} />
+          </Router>
+        </BoclipsClientProvider>,
+      );
 
-    expect(wrapper.queryByText('Constitution')).toBeNull();
-    expect(await wrapper.findByText('.S. Government')).toBeInTheDocument();
-    expect(await wrapper.findByText('.S. Senate')).toBeInTheDocument();
-  });
+      const searchInput = wrapper.getByPlaceholderText(
+        'Search for videos',
+      ) as HTMLInputElement;
+      fireEvent.change(searchInput, { target: { value: 'U' } });
+      fireEvent.focus(searchInput);
 
-  it(`does not display search auto-suggest if user does not have feature flag`, async () => {
-    const fakeBoclipsClient = new FakeBoclipsClient();
-    fakeBoclipsClient.users.setCurrentUserFeatures({
-      BO_WEB_APP_SEARCH_SUGGESTIONS: false,
-    });
-    const wrapper = render(
-      <BoclipsClientProvider client={fakeBoclipsClient}>
-        <Router history={createBrowserHistory()}>
-          <Search showIconOnly={false} />
-        </Router>
-      </BoclipsClientProvider>,
-    );
+      await waitFor(() => {
+        const suggestEvent = lastEvent(
+          fakeBoclipsClient,
+        ) as SearchQueryCompletionsSuggestedRequest;
 
-    fakeBoclipsClient.suggestions.populate({
-      channels: [],
-      subjects: [],
-      suggestionTerm: 'U.S. ',
-      phrases: ['U.S. Senate', 'U.S. Constitution'],
+        expect(suggestEvent?.impressions).toEqual([
+          'U.S. Senate',
+          'U.S. Government',
+        ]);
+        expect(suggestEvent?.searchQuery).toEqual('U');
+      });
     });
 
-    const searchInput = wrapper.getByPlaceholderText(
-      'Search for videos',
-    ) as HTMLInputElement;
-    fireEvent.change(searchInput, { target: { value: 'U.S.' } });
-    fireEvent.focus(searchInput);
+    it('sends one final event when search is performed by using a suggestion', async () => {
+      const wrapper = render(
+        <BoclipsClientProvider client={fakeBoclipsClient}>
+          <Router history={createBrowserHistory()}>
+            <Search showIconOnly={false} />
+          </Router>
+        </BoclipsClientProvider>,
+      );
 
-    await waitFor(() => {
-      expect(searchInput.value).toEqual('U.S.');
-      expect(wrapper.queryByText('Constitution')).toBeNull();
-    });
-  });
+      const searchInput = wrapper.getByPlaceholderText(
+        'Search for videos',
+      ) as HTMLInputElement;
+      fireEvent.change(searchInput, { target: { value: 'U' } });
+      fireEvent.focus(searchInput);
 
-  it('sends an event when auto-suggest displayed', async () => {
-    const fakeBoclipsClient = new FakeBoclipsClient();
-    fakeBoclipsClient.users.setCurrentUserFeatures({
-      BO_WEB_APP_SEARCH_SUGGESTIONS: false,
-    });
-    const wrapper = render(
-      <BoclipsClientProvider client={fakeBoclipsClient}>
-        <Router history={createBrowserHistory()}>
-          <Search showIconOnly={false} />
-        </Router>
-      </BoclipsClientProvider>,
-    );
+      expect(await wrapper.findByText('.S. Senate')).toBeInTheDocument();
+      fireEvent.click(await wrapper.findByText('.S. Senate'));
 
-    fakeBoclipsClient.suggestions.populate({
-      channels: [],
-      subjects: [],
-      suggestionTerm: 'U.S. ',
-      phrases: ['U.S. Senate', 'U.S. Constitution'],
-    });
+      await waitFor(() => {
+        const events: any[] = fakeBoclipsClient.events.getEvents();
+        const searchRelatedEvent: SearchQueryCompletionsSuggestedRequest =
+          events.find((e) => e.searchUrl);
 
-    const searchInput = wrapper.getByPlaceholderText(
-      'Search for videos',
-    ) as HTMLInputElement;
-    fireEvent.change(searchInput, { target: { value: 'U.S.' } });
-    fireEvent.focus(searchInput);
+        expect(searchRelatedEvent.searchQuery).toEqual('U');
+        expect(searchRelatedEvent.impressions).toEqual([
+          'U.S. Senate',
+          'U.S. Government',
+        ]);
 
-    await waitFor(() => {
-      const suggestEvent = lastEvent(
-        fakeBoclipsClient,
-      ) as SearchQueryCompletionsSuggestedRequest;
-
-      expect(suggestEvent?.impressions).toEqual([
-        'U.S. Senate',
-        'U.S. Constitution',
-      ]);
-      expect(suggestEvent?.searchQuery).toEqual('U.S.');
+        expect(searchRelatedEvent?.searchUrl).toContain(
+          '/videos?q=U.S.+Senate&page=1',
+        );
+      });
     });
   });
 });
