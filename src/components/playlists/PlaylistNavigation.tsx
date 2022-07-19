f20a00dc2856011454b110420b224d9ef3e6684c
Maciej Idzikowski <maciej@boclips.com>
2022-07-19 12:41:54 +0200

    Capitalize breadcrumb text
    [#182439980]
    
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- src/components/playlists/PlaylistNavigation.tsx
+++ src/components/playlists/PlaylistNavigation.tsx
@@ -13,7 +13,7 @@ const PlaylistNavigation = ({ playlist }: Props) => {
   return (
     <div className={c(s.playlistNavigation, 'order-first')}>
       <Link to="/library" data-qa="to-library-link">
-        Your library
+        Your Library
       </Link>
       <Arrow />
       <Link to={`/playlists/${playlist.id}`} data-qa="playlist-title-link">
