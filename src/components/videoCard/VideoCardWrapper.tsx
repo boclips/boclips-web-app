45a353c802cfcd0a00255b114d1173dc94405f17
Alex Glen <alex@boclips.com>
2022-07-21 17:34:00 +0100

    Bump bo-ui components to latest
    - remove boclips-api-client override, we don't seem to need it anymore
    
    [#182499277]

--- src/components/videoCard/VideoCardWrapper.tsx
+++ src/components/videoCard/VideoCardWrapper.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Video } from 'boclips-api-client/dist/sub-clients/videos/model/Video';
-import { convertVideoFromApi } from 'src/services/convertVideoFromApi';
 import { VideoCard } from '@boclips-ui/video-card';
 import { PriceBadge } from 'src/components/common/price/PriceBadge';
 import { VideoPlayer } from 'src/components/videoCard/VideoPlayer';
@@ -49,7 +48,7 @@ export const VideoCardWrapper = ({ video, handleFilterChange }: Props) => {
     <div className={s.videoCard}>
       <VideoCard
         key={video.id}
-        video={convertVideoFromApi(video)}
+        video={video}
         videoPlayer={<VideoPlayer video={video} showDurationBadge />}
         border="bottom"
         createdBy={createdByLink()}
