62f21f1b1b99d5a05f7eaf3d52bbba3123a967e7
Arfaz Feroz <arfaz@boclips.com>
2022-07-28 17:52:39 +0100

    Add link to explore page in navbar and add titles
    [#182760277]
    
    Signed-off-by: Alex Glen <alex@boclips.com>

--- src/views/explore/ExploreView.tsx
+++ src/views/explore/ExploreView.tsx
@@ -5,35 +5,40 @@ import Navbar from 'src/components/layout/Navbar';
 import Footer from 'src/components/layout/Footer';
 import { Layout } from 'src/components/layout/Layout';
 
-export const ExploreView = () => {
+const ExploreView = () => {
   const { data: books } = useGetBooksQuery();
   const subjects = new Set(books?.map((book) => book.subject));
   console.log(books);
   return (
-    <Layout rowsSetup="grid-rows-default-view-with-title" responsiveLayout>
+    <Layout rowsSetup="grid-rows-playlist-view" responsiveLayout>
       <Navbar />
-      <section className="col-start-2 col-end-26 grid-row-start-2 grid-row-end-2 flex flex-col">
-        <div className="w-full h-12 justify-center">
-          <Typography.H1>
-            Our best content aligned to OpenStax courses
-          </Typography.H1>
-        </div>
+      <Typography.H1
+        size="lg"
+        className="col-start-2 col-end-26 grid-row-start-2 grid-row-end-2 text-center"
+      >
+        Our best content aligned to OpenStax courses
+      </Typography.H1>
 
-        <div className="w-full h-12 justify-center">
-          <Typography.H2>
-            Review videos hand-picked by our curators and decide if they are
-            right for your course
-          </Typography.H2>
-        </div>
-        <div className="w-full h-12 justify-center">
-          {Array.from(subjects).map((subject) => {
-            return <Typography.Body> {subject} </Typography.Body>;
-          })}
-        </div>
-      </section>
-      <div className="col-start-2 col-end-26 grid-row-start-3 grid-row-end-3 flex flex-row">List of books</div>
+      <Typography.H2
+        size="md"
+        weight="regular"
+        className="col-start-2 col-end-26 grid-row-start-3 grid-row-end-3 text-center"
+      >
+        Review videos hand-picked by our curators and decide if they are right
+        for your course
+      </Typography.H2>
+      <div className="col-start-2 col-end-26 grid-row-start-4 grid-row-end-4 text-center">
+        {Array.from(subjects).map((subject) => {
+          return <Typography.Body> {subject} </Typography.Body>;
+        })}
+      </div>
+      <main className="col-start-2 col-end-26 grid-row-start-5 grid-row-end-5">
+        <span>List of books</span>
+      </main>
 
-      <Footer />
+      <Footer columnPosition="col-start-2 col-end-26" />
     </Layout>
   );
 };
+
+export default ExploreView;
