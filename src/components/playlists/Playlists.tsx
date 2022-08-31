56e3b3e7ee9f4f402f28d84d3027eb6ad5ebf9cf
Maciej Idzikowski <maciej@boclips.com>
2022-08-31 15:24:37 +0100

    Add pagination to playlist view
    [#183058668]
    
    Signed-off-by: Arfaz Feroz <arfaz@boclips.com>

--- src/components/playlists/Playlists.tsx
+++ src/components/playlists/Playlists.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useOwnAndSharedPlaylistsQuery } from 'src/hooks/api/playlistsQuery';
 import SkeletonTiles from 'src/components/skeleton/Skeleton';
 import Thumbnails from 'src/components/playlists/thumbnails/Thumbnails';
@@ -7,47 +7,103 @@ import { Constants } from 'src/AppConstants';
 import { Link } from 'react-router-dom';
 import { HotjarEvents } from 'src/services/analytics/hotjar/Events';
 import AnalyticsFactory from 'src/services/analytics/AnalyticsFactory';
-import GridCard from '../common/gridCard/GridCard';
+import List from 'antd/lib/list';
+import Pagination from '@boclips-ui/pagination';
+import c from 'classnames';
+import { ListViewCollection } from 'boclips-api-client/dist/sub-clients/collections/model/ListViewCollection';
+import { getMediaBreakpoint } from '@boclips-ui/use-media-breakpoints';
 import s from './style.module.less';
+import GridCard from '../common/gridCard/GridCard';
+import paginationStyles from '../common/pagination/pagination.module.less';
 
 const Playlists = () => {
+  const PAGE_SIZE = 20;
+  const currentBreakpoint = getMediaBreakpoint();
+  const mobileView = currentBreakpoint.type === 'mobile';
+
   const linkCopiedHotjarEvent = () =>
     AnalyticsFactory.hotjar().event(HotjarEvents.PlaylistLinkCopied);
 
   const { data: playlists, isLoading } = useOwnAndSharedPlaylistsQuery();
 
+  const [page, setPage] = useState<number>(1);
+
+  const handlePageChange = (newPage: number) => {
+    window.scrollTo({ top: 0 });
+    setPage(newPage);
+  };
+
+  const itemRender = React.useCallback(
+    (pageNb, type) => {
+      return (
+        <Pagination
+          buttonType={type}
+          page={pageNb}
+          mobileView={mobileView}
+          currentPage={page}
+          totalItems={Math.ceil(playlists.pageSpec.totalElements / PAGE_SIZE)}
+        />
+      );
+    },
+    [mobileView, page, playlists?.pageSpec.totalPages],
+  );
+
   return (
     <main tabIndex={-1} className={s.playlistsWrapper}>
       {isLoading ? (
-        <SkeletonTiles className={s.skeletonCard} />
+        <div className={s.skeletonWrapper}>
+          <SkeletonTiles className={s.skeletonCard} numberOfTiles={12} />
+        </div>
       ) : (
-        playlists?.map((playlist) => (
-          <GridCard
-            key={playlist.id}
-            link={`/playlists/${playlist.id}`}
-            name={playlist.title}
-            overlay={
-              playlist.mine === false && (
-                <div className={s.sharedWithYouOverlay}>Shared with you</div>
-              )
-            }
-            header={
-              <Link tabIndex={-1} to={`/playlists/${playlist.id}`}>
-                <Thumbnails videos={playlist.videos} />
-              </Link>
-            }
-            footer={
-              <div className="w-fit	self-end p-1">
-                <CopyButton
-                  ariaLabel="Copy playlist link"
-                  textToCopy={`${Constants.HOST}/playlists/${playlist.id}`}
-                  dataQa={`share-playlist-button-${playlist.id}`}
-                  onCopy={linkCopiedHotjarEvent}
-                />
-              </div>
-            }
-          />
-        ))
+        <List
+          className={s.gridView}
+          pagination={{
+            total: playlists.pageSpec.totalElements,
+            className: c(paginationStyles.pagination, s.pagination, {
+              [paginationStyles.paginationEmpty]: !playlists.page.length,
+            }),
+            hideOnSinglePage: true,
+            pageSize: PAGE_SIZE,
+            showSizeChanger: false,
+            onChange: handlePageChange,
+            current: page,
+            showLessItems: mobileView,
+            prefixCls: 'bo-pagination',
+            itemRender,
+          }}
+          dataSource={playlists.page}
+          renderItem={(playlist: ListViewCollection) => (
+            <li data-qa="playlist-card">
+              <GridCard
+                key={playlist.id}
+                link={`/playlists/${playlist.id}`}
+                name={playlist.title}
+                overlay={
+                  playlist.mine === false && (
+                    <div className={s.sharedWithYouOverlay}>
+                      Shared with you
+                    </div>
+                  )
+                }
+                header={
+                  <Link tabIndex={-1} to={`/playlists/${playlist.id}`}>
+                    <Thumbnails videos={playlist.videos} />
+                  </Link>
+                }
+                footer={
+                  <div className="w-fit	self-end p-1">
+                    <CopyButton
+                      ariaLabel="Copy playlist link"
+                      textToCopy={`${Constants.HOST}/playlists/${playlist.id}`}
+                      dataQa={`share-playlist-button-${playlist.id}`}
+                      onCopy={linkCopiedHotjarEvent}
+                    />
+                  </div>
+                }
+              />
+            </li>
+          )}
+        />
       )}
     </main>
   );
---
+++ src/views/library/LibraryViewPagination.integrationTest.tsx
@@ -0,0 +1,75 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import App from 'src/App';
+import { FakeBoclipsClient } from 'boclips-api-client/dist/test-support';
+import { stubBoclipsSecurity } from 'src/testSupport/StubBoclipsSecurity';
+import { UserFactory } from 'boclips-api-client/dist/test-support/UserFactory';
+import { BoclipsClient } from 'boclips-api-client';
+import { CollectionFactory } from 'src/testSupport/CollectionFactory';
+import { QueryClient } from 'react-query';
+
+const insertUser = (client: FakeBoclipsClient) =>
+  client.users.insertCurrentUser(UserFactory.sample());
+
+const renderLibraryView = (client: BoclipsClient) =>
+  render(
+    <MemoryRouter initialEntries={['/library']}>
+      <App
+        apiClient={client}
+        boclipsSecurity={stubBoclipsSecurity}
+        reactQueryClient={new QueryClient()}
+      />
+    </MemoryRouter>,
+  );
+
+describe('LibraryView', () => {
+  beforeEach(() => {
+    window.resizeTo(1680, 1024);
+  });
+
+  it('displays pagination when more than 20 playlists', async () => {
+    const client = new FakeBoclipsClient();
+    insertUser(client);
+    loadPlaylists(client, 25);
+
+    const wrapper = renderLibraryView(client);
+    expect(await wrapper.findByText('Playlist 1')).toBeVisible();
+    expect(
+      await wrapper.findByRole('button', { name: 'Next page' }),
+    ).toBeVisible();
+  });
+
+  it(`doesn't display pagination when less than 20 playlists`, async () => {
+    const client = new FakeBoclipsClient();
+    insertUser(client);
+    loadPlaylists(client, 15);
+
+    const wrapper = renderLibraryView(client);
+    expect(await wrapper.findByText('Playlist 1')).toBeVisible();
+    expect(
+      await wrapper.queryByRole('button', { name: 'Next page' }),
+    ).toBeNull();
+  });
+
+  it('renders playlists created by the user', async () => {
+    const client = new FakeBoclipsClient();
+    insertUser(client);
+    loadPlaylists(client, 10);
+
+    const wrapper = renderLibraryView(client);
+
+    expect(await wrapper.findByText('Playlist 1')).toBeVisible();
+    expect(await wrapper.findByText('Playlist 2')).toBeVisible();
+  });
+
+  function loadPlaylists(client: FakeBoclipsClient, quantity: number) {
+    Array.from(Array(quantity).keys()).forEach((i) => {
+      const playlist = CollectionFactory.sample({
+        id: `${i}`,
+        title: `Playlist ${i}`,
+      });
+      client.collections.addToFake(playlist);
+    });
+  }
+});
