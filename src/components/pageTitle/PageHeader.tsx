c532f14063ee7bc3aebdd8086fda8981c9c3c46b
Matt Jones <matt@boclips.com>
2022-09-22 11:15:12 +0100

    Add labelledby on page header
    This satisfies using landmarks to identify regions of a page
    
    [#183187514]

--- src/components/pageTitle/PageHeader.tsx
+++ src/components/pageTitle/PageHeader.tsx
@@ -10,8 +10,9 @@ interface Props {
 
 const PageHeader = ({ title, button, cartItems }: Props) => {
   return (
-    <section className={s.header}>
+    <section className={s.header} aria-labelledby="page-header">
       <Typography.H1
+        id="page-header"
         size="md"
         title={title}
         className="text-gray-900 mb-2 lg:mb-0"
