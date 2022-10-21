59c89274d915296f0d3432a56103e6a48b71147e
Maciej Idzikowski <maciej@boclips.com>
2022-10-21 14:46:59 +0200

    Another layout fix to keep footer on the bottom
    [#183298159]
    
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,10 +33,10 @@ module.exports = {
         'default-view': 'auto minmax(0, auto) auto',
         'search-view': 'auto repeat(3, auto) auto',
         'default-view-with-title':
-          'auto minmax(50px, auto) minmax(auto, 100%) auto',
+          'auto minmax(50px, auto) auto minmax(auto,100%) auto',
         'playlist-view':
           'auto minmax(50px, auto) auto auto minmax(0, auto) auto',
-        'explore-view': 'repeat(3, auto) minmax(auto, 100%) auto',
+        'explore-view': 'repeat(3, auto) auto minmax(auto,100%) auto',
         'openstax-detailed-view': 'repeat(4, auto)',
         'order-view': 'auto 21px minmax(0, auto) minmax(0, auto) auto',
         'video-view': 'auto minmax(0, auto) repeat(5,auto)',
