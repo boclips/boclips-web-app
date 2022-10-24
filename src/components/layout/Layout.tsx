4e3e2ceeeec8a531c0a5918a1ca0a317e511a9a4
Remy <remy.o@outlook.com>
2022-10-24 12:30:57 +0200

    Revert "Fix layout config to keep footer at the bottom"
    This reverts commit 44bd6d4439185f896bb19e57918400e80358292c.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -16,7 +16,7 @@ export const Layout = ({
 }: Props) => (
   <div
     data-qa={dataQa}
-    className={`grid ${rowsSetup} grid-cols-container gap-y-6 gap-x-2 lg:gap-x-6 h-screen`}
+    className={`grid ${rowsSetup} grid-cols-container gap-y-6 gap-x-2 lg:gap-x-6`}
     style={{ minWidth: responsiveLayout ? '320px' : '1160px' }}
   >
     {children}
