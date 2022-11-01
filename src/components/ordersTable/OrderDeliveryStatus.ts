75a5f57152fb8ca93eee9d9e521481570e4896d0
Grzegorz Tatarzyn <greg@boclips.com>
2022-11-01 16:27:27 +0100

    Fix order status mappings

--- src/components/ordersTable/OrderDeliveryStatus.ts
+++ src/components/ordersTable/OrderDeliveryStatus.ts
@@ -1,10 +1,10 @@
 import { OrderStatus } from 'boclips-api-client/dist/sub-clients/orders/model/Order';
 
 export const orderDeliveryStatus = new Map([
-  [OrderStatus.READY, 'PROCESSING'],
-  [OrderStatus.INVALID, 'PROCESSING'],
+  [OrderStatus.READY, 'READY'],
+  [OrderStatus.INVALID, 'ERROR'],
   [OrderStatus.IN_PROGRESS, 'PROCESSING'],
   [OrderStatus.INCOMPLETED, 'PROCESSING'],
-  [OrderStatus.CANCELLED, 'PROCESSING'],
-  [OrderStatus.DELIVERED, 'DELIVERED'],
+  [OrderStatus.CANCELLED, 'CANCELLED'],
+  [OrderStatus.DELIVERED, 'PROCESSING'],
 ]);
