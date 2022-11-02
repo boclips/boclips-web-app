0ea74c5c8cc6d3eadc66cfd76a599a5a94a74d3f
Marcin Janik <marcin@boclips.com>
2022-11-02 19:01:10 +0100

    Add revenue to summary block

--- src/components/dashboard/SummaryBlock.tsx
+++ src/components/dashboard/SummaryBlock.tsx
@@ -1,16 +1,28 @@
 import { Typography } from '@boclips-ui/typography';
 import React from 'react';
 import { Revenue } from 'src/hooks/api/dashboard/Revenue';
+import MoneyRainIcon from 'src/resources/icons/money-rain.svg';
 
 interface Props {
   revenueData: Revenue;
 }
 export const SummaryBlock = ({ revenueData }: Props) => {
   return (
-    <div className=" flex justify-center">
-      <Typography.H2 size="sm" className="text-gray-700">
-        {revenueData?.revenue[0].revenue || 0}
-      </Typography.H2>
-    </div>
+    revenueData && (
+      <div className="flex justify-center flex-col justify-between space-y-4 p-6">
+        <Typography.H3>
+          Congratulations {revenueData.contractName}!
+        </Typography.H3>
+        <Typography.H5 size="xs" className="text-gray-500">
+          Your revenue so far:
+        </Typography.H5>
+        <div className="flex flex-row content-between content-center items-center justify-between pb-4 border-gray-300 border-b-2">
+          <Typography.H1 className="text-green-600">
+            ${revenueData.revenue[0].revenue}
+          </Typography.H1>
+          <MoneyRainIcon />
+        </div>
+      </div>
+    )
   );
 };
