e6c351d731144366bdbfbb3e6a6b8a8896a3f14e
Rafal Lewandowski <rafal@boclips.com>
2022-11-03 11:51:29 +0100

    Stuff

--- src/components/slidingDrawer/DrawerVideo.tsx
+++ src/components/slidingDrawer/DrawerVideo.tsx
@@ -2,6 +2,7 @@ import { Video } from 'boclips-api-client/dist/sub-clients/videos/model/Video';
 import { VideoPlayer } from 'src/components/videoCard/VideoPlayer';
 import React from 'react';
 import Button from '@boclips-ui/button';
+import s from './DrawerVideoList.module.less';
 
 interface Props {
   video: Video;
@@ -12,6 +13,7 @@ const DrawerVideo = ({ video, onAddPressed }: Props) => {
   return (
     <>
       <VideoPlayer video={video} />
+      <span className={s.text}>{video.title}</span>
       <span className="pt-2">
         <Button
           height="34px"
