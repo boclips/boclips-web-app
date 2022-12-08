aa904756d898942e2c38052b384765913b65c176
Remigiusz Oleszczuk <remy@boclips.com>
2022-12-08 14:54:54 +0100

    Update cypress config
    [#183795406]

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   viewportWidth: 1280,
+  viewportHeight: 1024,
   e2e: {
     baseUrl: 'http://localhost:9000',
   },
