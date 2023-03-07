b35bbcef280e64e2e17b43359a6a37b65e341743
Remigiusz Oleszczuk <remy@boclips.com>
2023-03-07 16:43:18 +0100

    Webpack improvements
    - use react-refresh reload, from now on updating pages will not require
      page reload
    - reloads html/js/css
    - add cache for even faster build times
    
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,38 +1,39 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common');
-
-const srcPath = path.resolve(__dirname, '../src');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
-  entry: srcPath,
+  entry: path.resolve(__dirname, '../src'),
   output: {
     filename: '[name].js',
     publicPath: '/',
     path: path.resolve(__dirname, '../dist'),
   },
   devServer: {
-    static: srcPath,
+    static: path.join(__dirname, '../src/resources/'),
     port: 9000,
     host: 'localhost',
     hot: true,
-    https: false,
-    historyApiFallback: true,
+    client: { overlay: false },
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].css', ignoreOrder: true }),
     new HtmlWebpackPlugin({
       inject: false,
       filename: 'silent-check-sso.html',
-      template: path.resolve(srcPath, 'silent-check-sso.html'),
+      template: path.resolve(
+        path.resolve(__dirname, '../src'),
+        'silent-check-sso.html',
+      ),
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.resolve(srcPath, 'index-dev.html'),
+      template: path.resolve(
+        path.resolve(__dirname, '../src'),
+        'index-dev.html',
+      ),
     }),
   ],
 });
