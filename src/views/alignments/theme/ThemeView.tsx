076105b1ca39c765019e10372bd0470e0ef9656f
Maciej Idzikowski <maciej@boclips.com>
2023-03-24 13:13:50 +0100

    Load providers using query in ThemeView
    [#184580407]
    
    Signed-off-by: Rafal Lewandowski <rafal@boclips.com>

--- src/views/alignments/theme/ThemeView.tsx
+++ src/views/alignments/theme/ThemeView.tsx
@@ -3,50 +3,63 @@ import { Layout } from 'src/components/layout/Layout';
 import Navbar from 'src/components/layout/Navbar';
 import Footer from 'src/components/layout/Footer';
 import { useParams } from 'react-router';
-import { useGetThemeByProviderAndId } from 'src/hooks/api/alignmentsQuery';
+import {
+  useGetProvidersQuery,
+  useGetThemeByProviderAndId,
+} from 'src/hooks/api/alignmentsQuery';
 import { Content } from 'src/components/sparks/themePage/theme/Content';
 import { NavigationPanel } from 'src/components/sparks/themePage/navigationPanel/NavigationPanel';
 import { ThemeMobileMenuProvider } from 'src/components/common/providers/ThemeMobileMenuProvider';
 import ThemeSkeletonPage from 'src/components/skeleton/theme/ThemeSkeletonPage';
 import { Helmet } from 'react-helmet';
 import PaginationPanel from 'src/components/sparks/themePage/theme/pagination/PaginationPanel';
-import { ProviderFactory } from 'src/views/alignments/provider/ProviderFactory';
 import NotFound from 'src/views/notFound/NotFound';
 import { AlignmentContextProvider } from 'src/components/common/providers/AlignmentContextProvider';
 
 const ThemeView = () => {
-  const { id, provider: providerName } = useParams();
-  const { data: theme, isLoading } = useGetThemeByProviderAndId(
-    providerName,
+  const { id, provider: providerNavigationPath } = useParams();
+  const { data: theme, isLoading: isThemeLoading } = useGetThemeByProviderAndId(
+    providerNavigationPath,
     id,
   );
+  const { data: providers, isLoading: areProvidersLoading } =
+    useGetProvidersQuery();
 
-  const provider = ProviderFactory.isProviderSupported(providerName)
-    ? ProviderFactory.sample(providerName)
-    : undefined;
+  const provider = providers.find(
+    (it) => it.navigationPath === providerNavigationPath,
+  );
 
-  return provider && (isLoading || theme) ? (
-    <>
-      {theme?.title && <Helmet title={theme.title} />}
-      <Layout
-        rowsSetup="grid-rows-theme-detailed-view items-start"
-        responsiveLayout
-      >
-        <Navbar />
-        {isLoading ? (
-          <ThemeSkeletonPage />
-        ) : (
-          <ThemeMobileMenuProvider>
-            <AlignmentContextProvider provider={provider}>
-              <NavigationPanel theme={theme} />
-              <Content theme={theme} />
-              <PaginationPanel theme={theme} />
-            </AlignmentContextProvider>
-          </ThemeMobileMenuProvider>
-        )}
-        <Footer />
-      </Layout>
-    </>
+  const isLoading = isThemeLoading || areProvidersLoading;
+
+  const themeViewLayout = (children: React.ReactNode) => {
+    return (
+      <>
+        {theme?.title && <Helmet title={theme.title} />}
+        <Layout
+          rowsSetup="grid-rows-theme-detailed-view items-start"
+          responsiveLayout
+        >
+          <Navbar />
+          {children}
+          <Footer />
+        </Layout>
+      </>
+    );
+  };
+
+  if (isLoading) {
+    return themeViewLayout(<ThemeSkeletonPage />);
+  }
+  return provider && theme ? (
+    themeViewLayout(
+      <ThemeMobileMenuProvider>
+        <AlignmentContextProvider provider={provider}>
+          <NavigationPanel theme={theme} />
+          <Content theme={theme} />
+          <PaginationPanel theme={theme} />
+        </AlignmentContextProvider>
+      </ThemeMobileMenuProvider>,
+    )
   ) : (
     <NotFound />
   );
