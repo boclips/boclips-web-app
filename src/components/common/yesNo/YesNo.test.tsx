356c3892231a8274242aa6bc68ea3d3cfd0420c2
Rafal Lewandowski <rafal@boclips.com>
2023-05-29 15:26:50 +0200

    Provide a Yes/No input component
    Not used in the form just yet.
    
    [#185089850]
    
    Signed-off-by: Remigiusz Oleszczuk <remy@boclips.com>

---
+++ src/components/common/yesNo/YesNo.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import YesNo from 'src/components/common/yesNo/YesNo';
+import userEvent from '@testing-library/user-event';
+
+describe('YesNo', () => {
+  it('displays the provided label', () => {
+    const wrapper = render(
+      <form>
+        <YesNo
+          id="the-question"
+          label="Should I eat a donut?"
+          onValueChange={jest.fn()}
+        />
+      </form>,
+    );
+
+    expect(wrapper.getByText('Should I eat a donut?')).toBeVisible();
+  });
+
+  it('yields value changes', async () => {
+    const valueChangeHandler = jest.fn();
+    const wrapper = render(
+      <form>
+        <YesNo
+          id="the-question"
+          label="Should I eat a donut?"
+          onValueChange={valueChangeHandler}
+        />
+      </form>,
+    );
+
+    await userEvent.click(wrapper.getByLabelText('Yes'));
+    expect(valueChangeHandler).toHaveBeenCalledWith(true);
+    await userEvent.click(wrapper.getByLabelText('No'));
+    expect(valueChangeHandler).toHaveBeenLastCalledWith(false);
+  });
+});
---
+++ src/components/common/yesNo/YesNo.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { Typography } from '@boclips-ui/typography';
+import s from './style.module.less';
+
+export interface Props {
+  id: string;
+  label: string;
+  onValueChange: (value: boolean) => void;
+}
+
+const YesNo = ({ id, label, onValueChange }: Props) => {
+  const stringToBooleanValueChangeHandler = (stringValue: string) => {
+    if (stringValue === 'yes') {
+      onValueChange(true);
+    } else if (stringValue === 'no') {
+      onValueChange(false);
+    }
+  };
+
+  const yesId = `${id}-yes`;
+  const noId = `${id}-no`;
+
+  return (
+    <div className={s.wrapper}>
+      <span>{label}</span>
+      <RadioGroup.Root
+        className={s.radioGroupRoot}
+        orientation="horizontal"
+        onValueChange={stringToBooleanValueChangeHandler}
+      >
+        <div className={s.radioGroupItemWrapper}>
+          <RadioGroup.Item
+            className={s.radioGroupItem}
+            value="yes"
+            id={yesId}
+            aria-label={`${label} Yes`}
+          >
+            <RadioGroup.Indicator className={s.radioGroupIndicator} />
+          </RadioGroup.Item>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor={yesId}>
+            <Typography.Body as="span" className={s.radioItemLabel}>
+              Yes
+            </Typography.Body>
+          </label>
+        </div>
+        <div className={s.radioGroupItemWrapper}>
+          <RadioGroup.Item
+            className={s.radioGroupItem}
+            value="no"
+            id={noId}
+            aria-label={`${label} No`}
+          >
+            <RadioGroup.Indicator className={s.radioGroupIndicator} />
+          </RadioGroup.Item>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor={noId}>
+            <Typography.Body as="span" className={s.radioItemLabel}>
+              No
+            </Typography.Body>
+          </label>
+        </div>
+      </RadioGroup.Root>
+    </div>
+  );
+};
+
+export default YesNo;
