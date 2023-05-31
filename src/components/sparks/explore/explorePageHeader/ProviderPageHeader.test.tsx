51d93a31ee81e5ee2c6ce7f85f8a36f299c59340
Eva Varga <eva@boclips.com>
2023-05-31 10:48:04 +0100

    Allow users to navigate back to Sparks easily
    [#185237031]

--- src/components/sparks/explore/explorePageHeader/ProviderPageHeader.test.tsx
+++ src/components/sparks/explore/explorePageHeader/ProviderPageHeader.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ProviderPageHeader from 'src/components/sparks/explore/explorePageHeader/ProviderPageHeader';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { ProviderFactory } from 'src/views/alignments/provider/ProviderFactory';
 import { AlignmentContextProvider } from 'src/components/common/providers/AlignmentContextProvider';
+import * as router from 'react-router';
 
 describe('Provider Page page Header', () => {
+  const navigate = jest.fn();
+
+  beforeAll(() => {
+    jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate);
+  });
+
   it('displays header text and an OpenStax ally logo', () => {
     const header = render(
       <AlignmentContextProvider provider={ProviderFactory.sample('openstax')}>
@@ -19,5 +26,16 @@ describe('Provider Page page Header', () => {
       ),
     ).toBeVisible();
     expect(header.getByAltText('OpenStax logo')).toBeVisible();
+  });
+
+  it('shows a Back button which redirects to Sparks page', () => {
+    const header = render(
+      <AlignmentContextProvider provider={ProviderFactory.sample('openstax')}>
+        <ProviderPageHeader />
+      </AlignmentContextProvider>,
+    );
+
+    fireEvent.click(header.getByRole('button', { name: 'Back' }));
+    expect(navigate).toHaveBeenCalledWith('/sparks');
   });
 });
--- src/components/sparks/explore/explorePageHeader/ProviderPageHeader.tsx
+++ src/components/sparks/explore/explorePageHeader/ProviderPageHeader.tsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import { Typography } from '@boclips-ui/typography';
 import { useAlignmentProvider } from 'src/components/common/providers/AlignmentContextProvider';
+import { TextButton } from 'src/components/common/textButton/TextButton';
+import BackArrow from 'src/resources/icons/back-arrow.svg';
+import { useNavigate } from 'react-router-dom';
 import s from './style.module.less';
 
 const ProviderPageHeader = () => {
+  const navigate = useNavigate();
   const provider = useAlignmentProvider();
+
+  const goToSparks = () => {
+    navigate('/sparks');
+  };
+
   return (
     <section
-      className="col-start-2 col-end-26 grid-row-start-2 grid-row-end-2 mt-2 flex flex-col md:flex-row"
+      className="col-start-2 col-end-26 grid-row-start-2 grid-row-end-2 mt-2 flex flex-col"
       aria-labelledby="page-header"
     >
-      <div className="flex grow flex-col space-y-2 md:space-y-4 mb-4">
-        <Typography.H1 size="lg" id="page-header">
-          Our {provider.name} collection
-        </Typography.H1>
+      <TextButton onClick={goToSparks} text="Back" icon={<BackArrow />} />
+      <div className="flex flex-col md:flex-row">
+        <div className="flex grow flex-col space-y-2 md:space-y-4 mb-4">
+          <Typography.H1 size="lg" id="page-header">
+            Our {provider.name} collection
+          </Typography.H1>
 
-        <Typography.H2 size="xs" weight="regular">
-          {provider.description}
-        </Typography.H2>
+          <Typography.H2 size="xs" weight="regular">
+            {provider.description}
+          </Typography.H2>
+        </div>
+        <img
+          alt={`${provider.name} logo`}
+          className={s.providerLogo}
+          src={provider.logoUrl}
+        />
       </div>
-      <img
-        alt={`${provider.name} logo`}
-        className={s.providerLogo}
-        src={provider.logoUrl}
-      />
     </section>
   );
 };
