67008af1bfd6ec25c208530d9f56ac98708c9b94
Remigiusz Oleszczuk <remy@boclips.com>
2023-07-13 07:48:46 +0200

    Add create featured playlists to bo fake api for percy
    [#185302492]
    
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- src/testSupport/bo.ts
+++ src/testSupport/bo.ts
@@ -30,6 +30,7 @@ export interface Bo {
     cartWithVideos: () => void;
     emptyPlaylist: () => void;
     playlistWithVideos: () => void;
+    featuredPlaylists: () => void;
   };
   inspect: () => FakeBoclipsClient;
   set: {
@@ -149,6 +150,30 @@ export function bo(apiClient: FakeBoclipsClient): Bo {
     create: {
       video: boCreateVideo,
       subject: boCreateSubject,
+
+      featuredPlaylists: async () => {
+        apiClient.videos.insertVideo(
+          VideoFactory.sample({
+            id: '5f75b73f22a6495bdf2c2d14',
+            releasedOn: new Date(2011, 11, 1),
+            title:
+              'TED-Ed: No one can figure out how eels have sex | Lucy Cooke',
+            promoted: true,
+          }),
+        );
+
+        const collectionId = await apiClient.collections.create({
+          title: 'My featured playlist',
+          description: 'My playlist description',
+          videos: [
+            '5f75b73f22a6495bdf2c2d14',
+            '5f75b73f22a6495bdf2c2d14',
+            '5f75b73f22a6495bdf2c2d14',
+          ],
+        });
+
+        apiClient.collections.update(collectionId, { promoted: true });
+      },
 
       cartWithVideos: () => {
         apiClient.videos.insertVideo(
