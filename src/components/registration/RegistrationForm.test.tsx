d2e3d46e44437f1876984ff7b76c54d4232cdb9b
Maciej Idzikowski <maciej@boclips.com>
2023-09-22 12:46:11 +0200

    Fill the state of registration form when values typed
    [#186033346]
    
    Signed-off-by: Arfaz Feroz <arfaz@boclips.com>

--- src/components/registration/RegistrationForm.test.tsx
+++ src/components/registration/RegistrationForm.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import RegistrationForm from 'src/components/registration/RegistrationForm';
 
 describe('Registration Form', () => {
   it('renders the form', async () => {
-    const wrapper = render(<RegistrationForm />);
+    const wrapper = render(<RegistrationForm onSubmit={jest.fn()} />);
 
     expect(wrapper.getByText('CourseSpark')).toBeVisible();
     expect(wrapper.getByText('Create new account')).toBeVisible();
@@ -21,6 +21,40 @@ describe('Registration Form', () => {
         'By clicking Create Account, you agree to the Boclips User Agreement, Privacy Policy, and Cookie Policy.',
       ),
     ).toBeVisible();
-    expect(wrapper.getByRole('button', { name: 'Create Account' }));
+
+    expect(
+      wrapper.getByRole('button', { name: 'Create Account' }),
+    ).toBeVisible();
+  });
+
+  it('Typed values are submitted when clicked Create Account button ', async () => {
+    const onSubmit = jest.fn();
+    const wrapper = render(<RegistrationForm onSubmit={onSubmit} />);
+
+    fireEvent.change(wrapper.getByPlaceholderText('Your First name*'), {
+      target: { value: 'LeBron' },
+    });
+    fireEvent.change(wrapper.getByPlaceholderText('Your Last name*'), {
+      target: { value: 'James' },
+    });
+    fireEvent.change(wrapper.getByPlaceholderText('Your Professional Email*'), {
+      target: { value: 'lj@nba.com' },
+    });
+    fireEvent.change(wrapper.getByPlaceholderText('Password*'), {
+      target: { value: 'p@ss' },
+    });
+    fireEvent.change(wrapper.getByPlaceholderText('Confirm Password*'), {
+      target: { value: 'p@ss' },
+    });
+
+    fireEvent.click(wrapper.getByRole('button', { name: 'Create Account' }));
+
+    expect(onSubmit).toBeCalledWith({
+      firstName: 'LeBron',
+      lastName: 'James',
+      email: 'lj@nba.com',
+      password: 'p@ss',
+      confirmPassword: 'p@ss',
+    });
   });
 });
--- src/components/registration/RegistrationForm.tsx
+++ src/components/registration/RegistrationForm.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography } from '@boclips-ui/typography';
 import { InputText } from '@boclips-ui/input';
 import Button from '@boclips-ui/button';
 
-const RegistrationForm = () => {
+interface Props {
+  onSubmit: (registrationData: RegistrationData) => void;
+}
+
+interface RegistrationData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const RegistrationForm = ({ onSubmit }: Props) => {
+  const [registrationData, setRegistrationData] = useState<RegistrationData>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (fieldName, value) => {
+    if (!value) return;
+    setRegistrationData((prevState) => {
+      return {
+        ...prevState,
+        [fieldName]: value,
+      };
+    });
+  };
+
   return (
     <main tabIndex={-1} className="col-start-2 col-end-26">
       <section className="flex flex-col items-center">
@@ -13,32 +43,32 @@ const RegistrationForm = () => {
         <div className="flex flex-row">
           <InputText
             id="input-first-name"
-            onChange={() => {}}
+            onChange={(value) => handleChange('firstName', value)}
             inputType="text"
             placeholder="Your First name*"
           />
           <InputText
             id="input-last-name"
-            onChange={() => {}}
+            onChange={(value) => handleChange('lastName', value)}
             inputType="text"
             placeholder="Your Last name*"
           />
         </div>
         <InputText
           id="input-email"
-          onChange={() => {}}
-          inputType="text"
+          onChange={(value) => handleChange('email', value)}
+          inputType="email"
           placeholder="Your Professional Email*"
         />
         <InputText
           id="input-password"
-          onChange={() => {}}
+          onChange={(value) => handleChange('password', value)}
           inputType="text"
           placeholder="Password*"
         />
         <InputText
           id="input-confirm-password"
-          onChange={() => {}}
+          onChange={(value) => handleChange('confirmPassword', value)}
           inputType="text"
           placeholder="Confirm Password*"
         />
@@ -46,7 +76,12 @@ const RegistrationForm = () => {
           By clicking Create Account, you agree to the Boclips User Agreement,
           Privacy Policy, and Cookie Policy.
         </Typography.Body>
-        <Button onClick={() => {}} text="Create Account" />
+        <Button
+          onClick={() => {
+            onSubmit(registrationData);
+          }}
+          text="Create Account"
+        />
       </section>
     </main>
   );
