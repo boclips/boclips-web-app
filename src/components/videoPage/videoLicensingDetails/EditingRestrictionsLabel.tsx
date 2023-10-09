62af45f1335ab4e425f753eefabcf919306016c9
Arfaz7 <arfaz@boclips.com>
2023-10-09 10:12:53 +0100

    Add territory restrictions to video page (#567)
    * Add territory restrictions to video page
    
    [#185877110]
    
    * Refactor list of countries method
    
    [#185877110]

--- src/components/videoPage/videoLicensingDetails/EditingRestrictionsLabel.tsx
+++ src/components/videoPage/videoLicensingDetails/EditingRestrictionsLabel.tsx
@@ -1,23 +1,26 @@
 import React, { ReactElement } from 'react';
 import { Typography } from '@boclips-ui/typography';
 
-const editingPolicyLink = (
-  <a
-    rel="noopener noreferrer"
-    href="https://www.boclips.com/editing-policy"
-    target="_blank"
-  >
-    <Typography.Body size="small">
-      <Typography.Link type="inline-blue">
-        standard editing policy
-      </Typography.Link>
-    </Typography.Body>
-  </a>
-);
+interface Props {
+  permission: string;
+}
+export const EditingRestrictionsLabel = ({
+  permission,
+}: Props): string | ReactElement => {
+  const editingPolicyLink = (
+    <a
+      rel="noopener noreferrer"
+      href="https://www.boclips.com/editing-policy"
+      target="_blank"
+    >
+      <Typography.Body size="small">
+        <Typography.Link type="inline-blue">
+          standard editing policy
+        </Typography.Link>
+      </Typography.Body>
+    </a>
+  );
 
-export const getEditingRestrictionsLabel = (
-  permission: string,
-): string | ReactElement => {
   switch (permission) {
     case 'ALLOWED_WITH_RESTRICTIONS':
       return <>Additional restrictions apply as well as {editingPolicyLink}</>;
---
+++ src/components/videoPage/videoLicensingDetails/TerritoryRestrictionsLabel.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import InfoSVG from 'src/resources/icons/info.svg';
+import Tooltip from '@boclips-ui/tooltip';
+
+interface Props {
+  territories: string[];
+}
+
+export const TerritoryRestrictionsLabel = ({ territories }: Props) => {
+  const getLabel = () => {
+    if (territories.length === 1) {
+      return '1 country';
+    }
+    return `${territories.length} countries`;
+  };
+
+  return (
+    <p className="flex flex-row items-center gap-1">
+      <span>{getLabel()}</span>
+      <Tooltip text={territories.join(', ')}>
+        <button type="button" data-qa="territories-details">
+          <InfoSVG onClick={null} />
+        </button>
+      </Tooltip>
+    </p>
+  );
+};
