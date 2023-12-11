5c538624c3de1b9a69351272201d54e492d8aa3f
Remigiusz Oleszczuk <remy@boclips.com>
2023-12-11 13:41:35 +0100

    Reposition logo in /register page
    [#186473815]
    
    Signed-off-by: Marcin Janik <marcin@boclips.com>

--- src/components/registration/invisibleNavbar/InvisibleNavbar.tsx
+++ src/components/registration/invisibleNavbar/InvisibleNavbar.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import SkipLink from 'src/components/skipLink/SkipLink';
 import CourseSparkLogoSVG from 'src/resources/icons/coursespark.svg';
+import c from 'classnames';
 import s from './style.module.less';
 
 const InvisibleNavbar = () => {
   return (
-    <nav className="col-start-2 col-end-27 row-start-1 row-end-1">
+    <nav
+      className={c(
+        'col-start-2 col-end-27 row-start-1 row-end-1',
+        s.invisibleNavbar,
+      )}
+    >
       <CourseSparkLogoSVG className={s.logo} data-qa="logo" />
       <SkipLink />
     </nav>
