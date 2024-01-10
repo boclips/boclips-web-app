eaa991c8dcef70c0a1d7686562203bd407aa7a94
Maciej <106662168+Maciej-boclips@users.noreply.github.com>
2024-01-10 12:45:12 +0100

    E2e trial registration (#815)
    * Cypress test of registration form
    
    [#186177287]
    
    Signed-off-by: Remigiusz Oleszczuk <remy@boclips.com>

---
+++ cypress/e2e/RegistrationPage.cy.ts
@@ -0,0 +1,62 @@
+import { cleanupStyles } from 'cypress/mount-utils';
+
+context('Registration Page', () => {
+  it('Registration', () => {
+    cy.visit('/register');
+
+    cy.bo((bo) => {
+      bo.create.user();
+      bo.set.features({
+        BO_WEB_APP_DEV: true,
+      });
+      cy.window().then((window) => {
+        window.Environment.CAPTCHA_TOKEN =
+          '6Ldi6JwUAAAAACMgLoshiwBEZNDmnllXrAEIMN_y';
+      });
+
+      cy.intercept('POST', 'https://www.google.com/recaptcha/api2/reload**', {
+        statusCode: 200,
+        body: { success: true },
+      });
+
+      cy.window().then((win) => {
+        cy.stub(win.crypto, 'randomUUID').returns('123-123');
+      });
+    });
+
+    cy.findByText('Create your free account').should('be.visible');
+
+    cy.findByLabelText('Organization name').should('be.visible');
+    cy.findByLabelText('First name').should('be.visible');
+    cy.findByLabelText('Last name').should('be.visible');
+    cy.findByLabelText('Email Address').should('be.visible');
+    cy.findByText('Country').should('be.visible');
+    cy.findByLabelText('Password').should('be.visible');
+    cy.findByLabelText('Confirm password').should('be.visible');
+
+    cy.findByText(
+      'I certify that I am accessing this service solely for Educational Use.',
+    ).should('be.visible');
+
+    cy.findByRole('button', { name: 'Create Account' }).should('be.visible');
+
+    cy.percySnapshot('Trial Registration empty page');
+
+    cy.findByLabelText('Organization name').type('TestOrg');
+    cy.findByLabelText('First name').type('Bob');
+    cy.findByLabelText('Last name').type('Snail');
+    cy.findByLabelText('Email Address').type('bobsnail@boclips.com');
+    cy.findByRole('button', { name: 'Select country' }).click();
+    cy.findByText('Afghanistan').click();
+    cy.findByLabelText('Password').type('p@Ss1234');
+    cy.findByLabelText('Confirm password').type('p@Ss1234');
+
+    cy.findByText(/I certify that/).click();
+
+    cy.findByRole('button', { name: 'Create Account' }).click();
+
+    cy.findByText('Check your email!').should('be.visible');
+
+    cy.percySnapshot('Trial Registration Verification Prompt');
+  });
+});
