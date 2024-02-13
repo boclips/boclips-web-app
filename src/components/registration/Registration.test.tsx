2087a997ebda0c20a2afe9377ffcd5721f00b077
Harsha Chaudhary <harsha@boclips.com>
2024-02-13 18:10:46 +0530

    Hide trial info in registration mobile view
    [#186914075]

---
+++ src/components/registration/Registration.test.tsx
@@ -0,0 +1,48 @@
+import './mockRecaptcha';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { FakeBoclipsClient } from 'boclips-api-client/dist/test-support';
+import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BoclipsClientProvider } from 'src/components/common/providers/BoclipsClientProvider';
+import React from 'react';
+import { Registration } from 'src/components/registration/Registration';
+import {
+  resizeToDesktop,
+  resizeToMobile,
+  resizeToTablet,
+} from 'src/testSupport/resizeTo';
+
+describe('registration', () => {
+  it('displays trial info box in desktop view', async () => {
+    resizeToDesktop();
+    const wrapper = renderRegistration();
+
+    expect(await wrapper.findByText('Explore Boclips Library!')).toBeVisible();
+  });
+
+  it('does not displays trial info box in mobile view', async () => {
+    resizeToMobile();
+    const wrapper = renderRegistration();
+
+    expect(wrapper.queryByText('Explore Boclips Library!')).toBeNull();
+  });
+
+  it('does not displays trial info box in tablet view', async () => {
+    resizeToTablet();
+    const wrapper = renderRegistration();
+
+    expect(wrapper.queryByText('Explore Boclips Library!')).toBeNull();
+  });
+
+  const renderRegistration = () => {
+    return render(
+      <QueryClientProvider client={new QueryClient()}>
+        <BoclipsClientProvider client={new FakeBoclipsClient()}>
+          <GoogleReCaptchaProvider reCaptchaKey="123">
+            <Registration />
+          </GoogleReCaptchaProvider>
+        </BoclipsClientProvider>
+      </QueryClientProvider>,
+    );
+  };
+});
