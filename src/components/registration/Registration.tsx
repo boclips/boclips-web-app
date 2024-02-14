55aefbf915fea63670a5f5886539e68029c860ba
Harsha Chaudhary <harsha@boclips.com>
2024-02-14 14:17:20 +0530

    Hide trial info in registration mobile view
    [#186914075]
    
    Signed-off-by: Remigiusz Oleszczuk <remy@boclips.com>

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -4,17 +4,21 @@ import { Constants } from 'src/AppConstants';
 import RegistrationForm from 'src/components/registration/registrationForm/RegistrationForm';
 import EmailVerificationPrompt from 'src/components/registration/EmailVerificationPrompt';
 import TrialInfo from 'src/components/registration/trialInfo/TrialInfo';
+import { useMediaBreakPoint } from '@boclips-ui/use-media-breakpoints';
 
 const CAPTCHA_TOKEN = Constants.CAPTCHA_TOKEN;
 
 export const Registration = () => {
+  const breakpoints = useMediaBreakPoint();
+  const isMobileView =
+    breakpoints.type === 'mobile' || breakpoints.type === 'tablet';
   const [userEmailCreated, setUserEmailCreated] = useState<string>();
 
   return (
     <GoogleReCaptchaProvider reCaptchaKey={CAPTCHA_TOKEN}>
       {!userEmailCreated ? (
         <>
-          <TrialInfo />
+          {!isMobileView && <TrialInfo />}
           <RegistrationForm
             onRegistrationFinished={(userEmail) =>
               setUserEmailCreated(userEmail)
