264e9e04a48a12423e1b5bd1f5d2b4a2c6c39380
Harsha Chaudhary <harsha@boclips.com>
2024-02-29 16:23:07 +0530

    Homepage changes for classroom
    [#187119796]

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -4,6 +4,7 @@ import { Layout } from 'src/components/layout/Layout';
 import { Search } from 'src/components/searchBar/SearchBar';
 import Button from '@boclips-ui/button';
 import HomeSearchHero from 'src/resources/icons/home-illustration.svg';
+import HomeSearchHeroClassroom from 'src/resources/icons/classroom/home-search-hero.svg';
 import { useNavigate } from 'react-router-dom';
 import NewNavbar from 'src/components/layout/Navbar';
 import FeaturedPlaylists from 'src/components/featuredPlaylists/FeaturedPlaylists';
@@ -12,6 +13,8 @@ import { useMediaBreakPoint } from '@boclips-ui/use-media-breakpoints';
 import { useEmailVerified } from 'src/hooks/useEmailVerified';
 import useShowTrialWelcomeModal from 'src/hooks/useShowTrialWelcomeModal';
 import WelcomeModal from 'src/components/welcome/WelcomeModal';
+import { FeatureGate } from 'src/components/common/FeatureGate';
+import { Product } from 'boclips-api-client/dist/sub-clients/accounts/model/Account';
 import FilmIcon from '../../resources/icons/film-icon.svg';
 import YourLibraryIcon from '../../resources/icons/your-library.svg';
 import s from './style.module.less';
@@ -47,7 +50,13 @@ const HomeView = () => {
         className="row-start-2 row-end-2 col-start-2 col-end-26 pb-3 lg:pb-0 lg:pt-0 lg:col-start-4 lg:col-end-13"
       >
         <h1 className={s.header}>
-          Welcome to <span className={s.library}>Boclips Library</span>
+          Welcome to{' '}
+          <FeatureGate
+            product={Product.B2B}
+            fallback={<span className={s.classroom}>Boclips Classroom</span>}
+          >
+            <span className={s.library}>Boclips Library</span>
+          </FeatureGate>
         </h1>
         <Search showIconOnly={false} />
         <div className="mt-9 flex justify-between">
@@ -72,7 +81,17 @@ const HomeView = () => {
           className="row-start-2 row-end-2 col-start-17 mt-14 w-96 h-96"
           data-qa="home-search-hero"
         >
-          <HomeSearchHero />
+          <FeatureGate
+            product={Product.B2B}
+            fallback={
+              <div className={s.classroomHero}>
+                {' '}
+                <HomeSearchHeroClassroom />{' '}
+              </div>
+            }
+          >
+            <HomeSearchHero />
+          </FeatureGate>
         </div>
       )}
       <div className="row-start-3 row-end-3 col-start-2 col-end-26">
