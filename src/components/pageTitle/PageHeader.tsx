301cea7ee4fde8ae8f69ce9cbea66915681b7823
Harsha Chaudhary <harsha@boclips.com>
2024-03-06 20:40:36 +0530

    Add description to playlist page
    [#187173356]
    
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- src/components/pageTitle/PageHeader.tsx
+++ src/components/pageTitle/PageHeader.tsx
@@ -6,19 +6,25 @@ interface Props {
   title: string;
   button?: React.ReactNode;
   cartItems?: React.ReactNode;
+  description?: React.ReactNode;
 }
 
-const PageHeader = ({ title, button, cartItems }: Props) => {
+const PageHeader = ({ title, button, cartItems, description }: Props) => {
   return (
     <section className={s.header} aria-labelledby="page-header">
-      <Typography.H1
-        id="page-header"
-        size="md"
-        title={title}
-        className="text-gray-900 mb-2 lg:mb-0"
-      >
-        {title} {cartItems}
-      </Typography.H1>
+      <div className={s.title}>
+        <Typography.H1
+          id="page-header"
+          size="md"
+          title={title}
+          className="text-gray-900 mb-2 lg:mb-0"
+        >
+          {title} {cartItems}
+        </Typography.H1>
+        <div className={s.description}>
+          <Typography.Body>{description}</Typography.Body>
+        </div>
+      </div>
       {button}
     </section>
   );
