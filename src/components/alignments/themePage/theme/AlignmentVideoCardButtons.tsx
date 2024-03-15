e46585f95f4a9b0322a2a02812bf52d3eb815676
Marcin Janik <marcin@boclips.com>
2024-03-15 08:41:11 +0100

    Hide secondary button for Classroom users
    Classroom users will always have transcript button available within
    GridCard, so it does not need to be added specifically for alignments
    
    [#187237951]
    Signed-off-by: Ed Cook <edward@boclips.com>

--- src/components/alignments/themePage/theme/AlignmentVideoCardButtons.tsx
+++ src/components/alignments/themePage/theme/AlignmentVideoCardButtons.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Video } from 'boclips-api-client/dist/sub-clients/videos/model/Video';
 import { VideoCardButtons } from 'src/components/videoCard/buttons/VideoCardButtons';
 import { DownloadTranscriptButton } from 'src/components/downloadTranscriptButton/DownloadTranscriptButton';
+import { FeatureGate } from 'src/components/common/FeatureGate';
+import { Product } from 'boclips-api-client/dist/sub-clients/accounts/model/Account';
 
 interface Props {
   video: Video;
@@ -9,7 +11,9 @@ interface Props {
 
 export const AlignmentVideoCardButtons = ({ video }: Props) => {
   const secondaryButton = video.links.transcript && (
-    <DownloadTranscriptButton video={video} />
+    <FeatureGate product={Product.B2B}>
+      <DownloadTranscriptButton video={video} />
+    </FeatureGate>
   );
 
   return (
