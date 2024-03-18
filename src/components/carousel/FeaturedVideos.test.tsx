f0c5453ccb70588355c1642f22cabb7fd723dff4
Marcin Janik <marcin@boclips.com>
2024-03-18 11:02:03 +0100

    Display channel name in featured videos
    [#187239588]
    
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- src/components/carousel/FeaturedVideos.test.tsx
+++ src/components/carousel/FeaturedVideos.test.tsx
@@ -20,24 +20,28 @@ describe(`FeaturedVideos`, () => {
       VideoFactory.sample({
         id: '63fdfe8ad7fbb13615d23591',
         title: 'library video 1',
+        createdBy: 'One-T',
       }),
     );
     fakeApiClient.videos.insertVideo(
       VideoFactory.sample({
         id: '60cb11f60560d046cde9d1e9',
         title: 'library video 2',
+        createdBy: 'Nine-T',
       }),
     );
     fakeApiClient.videos.insertVideo(
       VideoFactory.sample({
         id: '627b417eef77c448ec160d95',
         title: 'library video 3',
+        createdBy: 'Bull-T',
       }),
     );
     fakeApiClient.videos.insertVideo(
       VideoFactory.sample({
         id: '63c04899bf161a652f79f0ed',
         title: 'library video 4',
+        createdBy: 'Me',
       }),
     );
 
@@ -45,24 +49,28 @@ describe(`FeaturedVideos`, () => {
       VideoFactory.sample({
         id: '642963c7f2ff6f79b7a3deee',
         title: 'classroom video 1',
+        createdBy: 'Reggie',
       }),
     );
     fakeApiClient.videos.insertVideo(
       VideoFactory.sample({
         id: '65e219d3acb34d232a464c4f',
         title: 'classroom video 2',
+        createdBy: 'Jay-Z',
       }),
     );
     fakeApiClient.videos.insertVideo(
       VideoFactory.sample({
         id: '62555fbb357dff6f7b1259bd',
         title: 'classroom video 3',
+        createdBy: '2pac and Biggie',
       }),
     );
     fakeApiClient.videos.insertVideo(
       VideoFactory.sample({
         id: '65153855ec623442504c7fa2',
         title: 'classroom video 4',
+        createdBy: 'André from OutKast',
       }),
     );
   });
@@ -84,9 +92,13 @@ describe(`FeaturedVideos`, () => {
 
     expect(await screen.findByText('Featured Videos')).toBeVisible();
     expect(await screen.findByText('library video 1')).toBeVisible();
+    expect(await screen.findByText('One-T')).toBeVisible();
     expect(await screen.findByText('library video 2')).toBeVisible();
+    expect(await screen.findByText('Nine-T')).toBeVisible();
     expect(await screen.findByText('library video 3')).toBeVisible();
+    expect(await screen.findByText('Bull-T')).toBeVisible();
     expect(await screen.findByText('library video 4')).toBeVisible();
+    expect(await screen.findByText('Me')).toBeVisible();
   });
 
   it(`should render title and 4 videos for Classroom user`, async () => {
@@ -106,8 +118,12 @@ describe(`FeaturedVideos`, () => {
 
     expect(await screen.findByText('Featured Videos')).toBeVisible();
     expect(await screen.findByText('classroom video 1')).toBeVisible();
+    expect(await screen.findByText('Reggie')).toBeVisible();
     expect(await screen.findByText('classroom video 2')).toBeVisible();
+    expect(await screen.findByText('Jay-Z')).toBeVisible();
     expect(await screen.findByText('classroom video 3')).toBeVisible();
+    expect(await screen.findByText('2pac and Biggie')).toBeVisible();
     expect(await screen.findByText('classroom video 4')).toBeVisible();
+    expect(await screen.findByText('André from OutKast')).toBeVisible();
   });
 });
