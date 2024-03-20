f631f2b0840034a8e95fed3cb4a452c614f82103
Alex Glen <alex@boclips.com>
2024-03-20 14:27:10 +0000

    Add analytics to bookmark modal
    [#187193970]

--- src/components/shareCodeButton/playlistsBookmark/bookmarkModal/BookmarkModal.test.tsx
+++ src/components/shareCodeButton/playlistsBookmark/bookmarkModal/BookmarkModal.test.tsx
@@ -17,20 +17,33 @@ import { ToastContainer } from 'react-toastify';
 import PlaylistVideoBookmarkButton from 'src/components/shareCodeButton/playlistsBookmark/PlaylistVideoBookmarkButton';
 import { Video } from 'boclips-api-client/dist/sub-clients/videos/model/Video';
 import { CollectionFactory } from 'src/testSupport/CollectionFactory';
+import { lastEvent } from 'src/testSupport/lastEvent';
 
 describe('Bookmark modal for playlists', () => {
-  it(`allows setting start and end time for video`, async () => {
-    const video = VideoFactory.sample({ id: 'video-id' });
-    const collection = CollectionFactory.sample({
-      videos: [VideoFactory.sample({ id: video.id })],
-      id: '123',
-    });
+  const video = VideoFactory.sample({ id: 'video-id' });
+  const collection = CollectionFactory.sample({
+    id: 'non-segment',
+    videos: [video],
+  });
+  const collectionWithSegment = CollectionFactory.sample({
+    id: 'has-segment',
+    videos: [video],
+    segments: {
+      'video-id': { start: 10, end: 11 },
+    },
+  });
+  const apiClient = new FakeBoclipsClient();
 
-    const apiClient = new FakeBoclipsClient();
+  beforeEach(() => {
+    apiClient.videos.clear();
+    apiClient.collections.clear();
 
     apiClient.videos.insertVideo(video);
     apiClient.collections.addToFake(collection);
+    apiClient.collections.addToFake(collectionWithSegment);
+  });
 
+  it(`allows setting start and end time for video`, async () => {
     const wrapper = renderBookmarkButton(video, collection.id, apiClient);
     await openShareModal(wrapper);
 
@@ -84,24 +97,11 @@ describe('Bookmark modal for playlists', () => {
   });
 
   it(`allows removing start and end time for video`, async () => {
-    const video = VideoFactory.sample({ id: '123' });
-    const collection = CollectionFactory.sample({
-      videos: [VideoFactory.sample({ id: video.id })],
-      id: '123',
-      segments: {
-        [video.id]: {
-          start: 1,
-          end: 10,
-        },
-      },
-    });
-
-    const apiClient = new FakeBoclipsClient();
-
-    apiClient.videos.insertVideo(video);
-    apiClient.collections.addToFake(collection);
-
-    const wrapper = renderBookmarkButton(video, '123', apiClient);
+    const wrapper = renderBookmarkButton(
+      video,
+      collectionWithSegment.id,
+      apiClient,
+    );
 
     await openShareModal(wrapper);
 
@@ -115,10 +115,12 @@ describe('Bookmark modal for playlists', () => {
     const startTimeInput = wrapper.getByLabelText('Start time:');
     const endTimeInput = wrapper.getByLabelText('End time:');
 
-    expect(endCheckbox).toBeChecked();
-    expect(startCheckbox).toBeChecked();
-    expect(startTimeInput).not.toBeDisabled();
-    expect(endTimeInput).not.toBeDisabled();
+    await waitFor(() => {
+      expect(startTimeInput).not.toBeDisabled();
+      expect(endTimeInput).not.toBeDisabled();
+      expect(endCheckbox).toBeChecked();
+      expect(startCheckbox).toBeChecked();
+    });
 
     await userEvent.click(startCheckbox);
     await userEvent.click(endCheckbox);
@@ -128,7 +130,9 @@ describe('Bookmark modal for playlists', () => {
 
     await userEvent.click(wrapper.getByText('Set'));
 
-    const updatedPlaylist = await apiClient.collections.get(collection.id);
+    const updatedPlaylist = await apiClient.collections.get(
+      collectionWithSegment.id,
+    );
 
     await waitFor(() => {
       expect(updatedPlaylist.segments[video.id]).toEqual(undefined);
@@ -183,21 +187,143 @@ describe('Bookmark modal for playlists', () => {
     ).toBeVisible();
   });
 
-  it('displays an error message if updating the bookmark fails', async () => {
-    const video = VideoFactory.sample({ id: '123' });
-    const collection = CollectionFactory.sample({
-      videos: [VideoFactory.sample({ id: video.id })],
-      id: '123',
+  it(`sends a platform interaction event when set bookmark modal is opened`, async () => {
+    const wrapper = renderBookmarkButton(video, collection.id, apiClient);
+    await openShareModal(wrapper);
+
+    expect(
+      await wrapper.findByText('Bookmark custom timings for this video'),
+    ).toBeVisible();
+
+    await waitFor(() => {
+      expect(lastEvent(apiClient, 'PLATFORM_INTERACTED_WITH')).toEqual({
+        type: 'PLATFORM_INTERACTED_WITH',
+        subtype: 'SET_BOOKMARK_MODAL_OPENED',
+        anonymous: false,
+      });
+    });
+  });
+  it(`sends a platform interaction event when update bookmark modal is opened`, async () => {
+    const wrapper = renderBookmarkButton(
+      video,
+      collectionWithSegment.id,
+      apiClient,
+    );
+    await openShareModal(wrapper);
+    await waitFor(() => {
+      expect(
+        wrapper.getByText('Update bookmarked timings for this video'),
+      ).toBeVisible();
     });
 
-    const apiClient = new FakeBoclipsClient();
-    apiClient.collections.addToFake(collection);
+    await waitFor(() => {
+      expect(lastEvent(apiClient, 'PLATFORM_INTERACTED_WITH')).toEqual({
+        type: 'PLATFORM_INTERACTED_WITH',
+        subtype: 'UPDATE_BOOKMARK_MODAL_OPENED',
+        anonymous: false,
+      });
+    });
+  });
 
+  it(`sends a platform interaction event when bookmark is set`, async () => {
+    const wrapper = renderBookmarkButton(video, collection.id, apiClient);
+    await openShareModal(wrapper);
+
+    expect(
+      await wrapper.findByText('Bookmark custom timings for this video'),
+    ).toBeVisible();
+
+    const startCheckbox = wrapper.getByRole('checkbox', {
+      name: 'Start time enabled',
+    });
+    const startTimeInput = wrapper.getByLabelText('Start time:');
+
+    await userEvent.click(startCheckbox);
+    fireEvent.change(startTimeInput, { target: { value: '00:32' } });
+
+    const endCheckbox = wrapper.getByRole('checkbox', {
+      name: 'End time enabled',
+      checked: false,
+    });
+    const endTimeInput = wrapper.getByLabelText('End time:');
+    await userEvent.click(endCheckbox);
+    fireEvent.change(endTimeInput, { target: { value: '00:55' } });
+
+    await userEvent.click(wrapper.getByText('Set'));
+    await waitFor(() => {
+      expect(
+        wrapper.getByText('Video bookmark has been updated'),
+      ).toBeInTheDocument();
+    });
+
+    const updatedPlaylist = await apiClient.collections.get(collection.id);
+
+    await waitFor(() => {
+      expect(updatedPlaylist?.segments[video.id].start).toEqual(32);
+      expect(updatedPlaylist?.segments[video.id].end).toEqual(55);
+      expect(lastEvent(apiClient, 'PLATFORM_INTERACTED_WITH')).toEqual({
+        type: 'PLATFORM_INTERACTED_WITH',
+        subtype: 'SET_BOOKMARK_MODAL_SUBMITTED',
+        anonymous: false,
+      });
+    });
+  });
+  it(`sends a platform interaction event when bookmark is updated`, async () => {
+    const wrapper = renderBookmarkButton(
+      video,
+      collectionWithSegment.id,
+      apiClient,
+    );
+    await openShareModal(wrapper);
+
+    expect(
+      await wrapper.findByText('Update bookmarked timings for this video'),
+    ).toBeVisible();
+    const startTimeInput = wrapper.getByLabelText('Start time:');
+
+    fireEvent.change(startTimeInput, { target: { value: '00:32' } });
+
+    const endTimeInput = wrapper.getByLabelText('End time:');
+    fireEvent.change(endTimeInput, { target: { value: '00:55' } });
+
+    await userEvent.click(wrapper.getByText('Set'));
+
+    const updatedPlaylist = await apiClient.collections.get(
+      collectionWithSegment.id,
+    );
+
+    await waitFor(() => {
+      expect(updatedPlaylist?.segments[video.id].start).toEqual(32);
+      expect(updatedPlaylist?.segments[video.id].end).toEqual(55);
+      expect(
+        wrapper.getByText('Video bookmark has been updated'),
+      ).toBeInTheDocument();
+      expect(lastEvent(apiClient, 'PLATFORM_INTERACTED_WITH')).toEqual({
+        type: 'PLATFORM_INTERACTED_WITH',
+        subtype: 'UPDATE_BOOKMARK_MODAL_SUBMITTED',
+        anonymous: false,
+      });
+    });
+  });
+
+  it('removes the tabIndex on main element, to allow copying the text', async () => {
+    const wrapper = renderBookmarkButton();
+
+    expect(wrapper.getByRole('main')).toHaveAttribute('tabIndex', '-1');
+
+    await openShareModal(wrapper);
+
+    await waitFor(() =>
+      expect(wrapper.getByRole('main')).not.toHaveAttribute('tabIndex'),
+    );
+  });
+
+  it('displays an error message if updating the bookmark fails', async () => {
     apiClient.collections.safeUpdate = jest
       .fn()
       .mockRejectedValue(new Error('Network Error'));
 
-    const wrapper = renderBookmarkButton(video, '123', apiClient);
+    const wrapper = renderBookmarkButton(video, collection.id, apiClient);
     await openShareModal(wrapper);
 
     const endCheckbox = wrapper.getByRole('checkbox', {
