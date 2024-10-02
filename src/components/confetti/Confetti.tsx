9f832721008378723b4284095b7988e5ac24dfc8
Ed Cook <edward@boclips.com>
2024-10-02 18:46:07 +0100

    Bump tsparticles deps and switch to v3 preset

--- src/components/confetti/Confetti.tsx
+++ src/components/confetti/Confetti.tsx
@@ -1,17 +1,23 @@
-import React, { useCallback, useMemo } from 'react';
-import Particles from 'react-particles';
-import type { Engine, ISourceOptions } from 'tsparticles-engine';
-import { loadConfettiPreset } from 'tsparticles-preset-confetti';
+import React, { useEffect, useMemo, useState } from 'react';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import type { Engine, ISourceOptions } from '@tsparticles/engine';
+import { loadConfettiPreset } from '@tsparticles/preset-confetti';
 import confettiSettings from './confetti-settings.json';
 
 const Confetti = () => {
-  const customInit = useCallback(async (engine: Engine) => {
-    await loadConfettiPreset(engine);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine: Engine) => {
+      await loadConfettiPreset(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
   // @ts-ignore
   const options: ISourceOptions = useMemo(() => confettiSettings, []);
 
-  return <Particles id="tsparticles" options={options} init={customInit} />;
+  return init && <Particles id="tsparticles" options={options} />;
 };
 export default Confetti;
