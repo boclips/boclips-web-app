14f11bebea0d1b8a4bba9093c2b2c80b04e6c2ac
Harsha Chaudhary <harsha@boclips.com>
2024-11-11 17:28:47 +0000

    Do not propagate errors from suggestions query
    Signed-off-by: Ed Cook <edward@boclips.com>

--- src/hooks/api/suggestionsQuery.ts
+++ src/hooks/api/suggestionsQuery.ts
@@ -6,8 +6,10 @@ const MINIMUM_SEARCH_LENGTH = 3;
 
 export const useGetSuggestionsQuery = (query: string) => {
   const client = useBoclipsClient();
-  return useQuery(['suggestions', query], async () =>
-    getSuggestionsQuery(query, client),
+  return useQuery(
+    ['suggestions', query],
+    async () => getSuggestionsQuery(query, client),
+    { useErrorBoundary: false },
   );
 };
 export const getSuggestionsQuery = (query: string, client: BoclipsClient) => {
