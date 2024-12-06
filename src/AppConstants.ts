a231b006f911c0966fe53c428acb243dbf14964a
Harsha Chaudhary <harsha@boclips.com>
2024-12-06 19:58:50 +0530

    Make env vars load, remove separate index files

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,3 +1,5 @@
+import * as process from 'process';
+
 declare global {
   interface Window {
     Environment: any;
@@ -14,31 +16,31 @@ export class AppConstants {
   }
 
   public get AUTH_ENDPOINT(): string {
-    return this.window.Environment.AUTH_ENDPOINT;
+    return import.meta.env.VITE_AUTH_ENDPOINT;
   }
 
   public get IS_HOTJAR_ENABLED(): boolean {
-    return this.window.Environment.IS_HOTJAR_ENABLED === 'true';
+    return import.meta.env.VITE_IS_HOTJAR_ENABLED === 'true';
   }
 
   public get CLASSROOM_ONBOARDING_VIDEO_ID(): string {
-    return this.window.Environment.CLASSROOM_ONBOARDING_VIDEO_ID;
+    return import.meta.env.VITE_CLASSROOM_ONBOARDING_VIDEO_ID;
   }
 
   public get IS_PENDO_ENABLED(): boolean {
-    return this.window.Environment.IS_PENDO_ENABLED === 'true';
+    return import.meta.env.VITE_IS_PENDO_ENABLED === 'true';
   }
 
   public get IS_SENTRY_ENABLED(): boolean {
-    return this.window.Environment.IS_SENTRY_ENABLED === 'true';
+    return import.meta.env.VITE_IS_SENTRY_ENABLED === 'true';
   }
 
   public get IS_HUBSPOT_ENABLED(): boolean {
-    return this.window.Environment.IS_HUBSPOT_ENABLED === 'true';
+    return import.meta.env.VITE_IS_HUBSPOT_ENABLED === 'true';
   }
 
   public get API_PREFIX(): string {
-    return this.window.Environment.API_PREFIX;
+    return import.meta.env.VITE_API_PREFIX;
   }
 
   public get PEARSON_ACCOUNT_ID(): string {
--- vite-dev.config.js
+++ vite-dev.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import image from '@rollup/plugin-image';
 import path from 'path';
@@ -49,6 +49,5 @@ export default defineConfig({
   server: {
     port: 9000,
     https: false,
-    open: 'index-dev.html',
   },
 });
