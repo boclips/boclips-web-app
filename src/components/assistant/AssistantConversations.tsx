d0ca7610ea55f82bfdc5d505a5a20ae5c31985f9
Ed Cook <edward@boclips.com>
2025-01-27 12:01:47 +0000

    Replace placeholder in sidebar with restart button
    [sc-2097]
    
    Signed-off-by: Harsha Chaudhary <harsha@boclips.com>

--- src/components/assistant/AssistantConversations.tsx
+++ src/components/assistant/AssistantConversations.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const AssistantConversations = () => {
-  return (
-    <div className="row-start-2 row-end-3 col-start-20 col-end-26">
-      Conversation Highlights
-    </div>
-  );
+  return <div className="row-start-2 row-end-3 col-start-20 col-end-26" />;
 };
 
 export default AssistantConversations;
--- src/components/assistant/AssistantSidebar.tsx
+++ src/components/assistant/AssistantSidebar.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import Button from '@boclips-ui/button';
+import s from 'src/components/assistant/chatInput/style.module.less';
+import { useAssistantContextProvider } from 'src/components/assistant/context/assistantContextProvider';
+import NewConversationIcon from 'resources/icons/new-conversation.svg';
 
 const AssistantSidebar = () => {
+  const { setConversationId, chatHistory, setChatHistory, setIsLoading } =
+    useAssistantContextProvider();
+
+  const handleReset = () => {
+    setChatHistory([]);
+    setConversationId('');
+    setIsLoading(false);
+  };
+
   return (
-    <div className="row-start-2 row-end-3 col-start-2 col-end-7">Sidebar</div>
+    <div className="row-start-2 row-end-3 col-start-2 col-end-7">
+      {chatHistory.length !== 0 ? (
+        <Button
+          type="outline"
+          className={s.newConversationButton}
+          height="48px"
+          width="auto"
+          onClick={handleReset}
+          text="Start new conversation"
+          icon={<NewConversationIcon />}
+        />
+      ) : null}
+    </div>
   );
 };
 
