f011480c15992587f57111ceaebadf79ee5ae53e
Ed Cook <edward@boclips.com>
2025-02-07 18:21:42 +0530

    Show segment bodal on highlight share
    [sc-2277]
    
    Signed-off-by: Harsha Chaudhary <harsha@boclips.com>

--- src/components/assistant/player/HighlightPlayer.tsx
+++ src/components/assistant/player/HighlightPlayer.tsx
@@ -11,15 +11,11 @@ import formatDuration from 'src/components/playlists/buttons/playlistBookmark/he
 import { useFindOrGetVideo } from 'src/hooks/api/videoQuery';
 import Thumbnail from 'src/components/playlists/thumbnails/Thumbnail';
 import { AddToPlaylistButton } from 'src/components/addToPlaylistButton/AddToPlaylistButton';
-import Button from '@boclips-ui/button';
-import { displayNotification } from 'src/components/common/notification/displayNotification';
 import { Product } from 'boclips-api-client/dist/sub-clients/accounts/model/Account';
-import ShareSVG from 'src/resources/icons/white-share.svg';
 import { FeatureGate } from 'src/components/common/FeatureGate';
-import { getShareableVideoLink } from 'src/components/shareLinkButton/getShareableLink';
-import { useGetUserQuery } from 'src/hooks/api/userQuery';
 import { EmbedButton } from 'src/components/embedButton/EmbedButton';
 import { useMediaBreakPoint } from '@boclips-ui/use-media-breakpoints';
+import { VideoShareLinkButton } from 'src/components/shareLinkButton/VideoShareLinkButton';
 import s from './style.module.less';
 
 interface Props {
@@ -28,7 +24,6 @@ interface Props {
 
 const HighlightPlayer = ({ clip }: Props) => {
   const client = useBoclipsClient();
-  const { data: user } = useGetUserQuery();
   const breakpoints = useMediaBreakPoint();
   const isMobileView =
     breakpoints.type === 'mobile' || breakpoints.type === 'tablet';
@@ -37,26 +32,6 @@ const HighlightPlayer = ({ clip }: Props) => {
   const { data: video, isLoading: isVideoLoading } = useFindOrGetVideo(
     clip.videoId,
   );
-
-  const handleShareCopy = () => {
-    const shareLink = getShareableVideoLink(
-      video.id,
-      user?.id,
-      clip.startTime,
-      clip.endTime,
-    );
-
-    client.shareLinks.trackVideoShareLink(video.id);
-
-    navigator.clipboard.writeText(shareLink).then(() => {
-      displayNotification(
-        'success',
-        'Share link copied!',
-        '',
-        'text-copied-notification',
-      );
-    });
-  };
 
   return (
     <div className={s.playerWrapper}>
@@ -94,18 +69,15 @@ const HighlightPlayer = ({ clip }: Props) => {
             iconOnly={isMobileView}
             outlineType={false}
           />
-          <FeatureGate product={Product.CLASSROOM}>
-            <Button
-              onClick={handleShareCopy}
-              dataQa="share-button"
-              text="Share"
-              aria-label="Share"
-              icon={<ShareSVG />}
-              iconOnly={isMobileView}
-              height="40px"
-              className={s.shareLinkButton}
-            />
-          </FeatureGate>
+          {!isVideoLoading && (
+            <FeatureGate product={Product.CLASSROOM}>
+              <VideoShareLinkButton
+                iconOnly={isMobileView}
+                video={video}
+                initialSegment={{ start: clip.startTime, end: clip.endTime }}
+              />
+            </FeatureGate>
+          )}
           {!isVideoLoading && video?.links?.createEmbedCode && (
             <EmbedButton
               video={video}
--- src/components/shareLinkButton/VideoShareLinkButton.tsx
+++ src/components/shareLinkButton/VideoShareLinkButton.tsx
@@ -4,31 +4,44 @@ import Button from '@boclips-ui/button';
 import { Video } from 'boclips-api-client/dist/types';
 import CopyLinkIcon from 'src/resources/icons/copy-link-icon.svg';
 import { useGetUserQuery } from 'src/hooks/api/userQuery';
-import { durationInSeconds } from 'src/components/cart/AdditionalServices/Trim/trimValidation';
+import {
+  durationInSeconds,
+  durationString,
+} from 'src/components/cart/AdditionalServices/Trim/trimValidation';
 import { Typography } from '@boclips-ui/typography';
 import { GoogleClassroomShareLink } from 'src/components/shareLinkButton/googleClassroom/GoogleClassroomShareLink';
 import { SegmentBodal } from 'src/components/segmentBodal/SegmentBodal';
 import { getShareableVideoLink } from 'src/components/shareLinkButton/getShareableLink';
 import { displayNotification } from 'src/components/common/notification/displayNotification';
 import { useBoclipsClient } from 'src/components/common/providers/BoclipsClientProvider';
+import { Segment } from 'boclips-api-client/dist/sub-clients/collections/model/Segment';
 import s from './shareLinkButton.module.less';
 
 interface VideoShareLinkButtonProps {
   iconOnly?: boolean;
   video: Video;
+  initialSegment?: Segment;
 }
 
 export const VideoShareLinkButton = ({
   iconOnly = false,
   video,
+  initialSegment,
 }: VideoShareLinkButtonProps) => {
   const client = useBoclipsClient();
-  const videoDuration = video.playback.duration.format('mm:ss');
+  const startingSegment = initialSegment ?? {
+    start: 0,
+    end: video.playback.duration.asSeconds(),
+  };
 
   const { data: user } = useGetUserQuery();
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [startDuration, setStartDuration] = useState('00:00');
-  const [endDuration, setEndDuration] = useState(videoDuration);
+  const [startDuration, setStartDuration] = useState(
+    durationString(startingSegment.start),
+  );
+  const [endDuration, setEndDuration] = useState(
+    durationString(startingSegment.end),
+  );
   const [isError, setIsError] = useState(false);
 
   const shareLink = getShareableVideoLink(
