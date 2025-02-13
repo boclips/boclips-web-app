53d163e517d8b7efd02488887db0413b537afe84
Harsha Chaudhary <harsha@boclips.com>
2025-02-13 18:35:46 +0530

    Remove direct dep on api client in Highlight player
    [sc-2318]

--- src/components/assistant/player/HighlightPlayer.tsx
+++ src/components/assistant/player/HighlightPlayer.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Feedback from 'src/components/assistant/feedback/Feedback';
-import { Link } from 'boclips-api-client/dist/sub-clients/common/model/LinkEntity';
-import expandUrlTemplate from 'boclips-api-client/dist/sub-clients/common/utils/expandUrlTemplate';
 import { VideoPlayer } from 'src/components/videoCard/VideoPlayer';
 import { Clip } from 'boclips-api-client/dist/sub-clients/chat/model/Clip';
-import { useBoclipsClient } from 'src/components/common/providers/BoclipsClientProvider';
 import Open from 'src/resources/icons/open-new-window.svg';
 import formatDuration from 'src/components/playlists/buttons/playlistBookmark/helpers/formatDuration';
 import { useFindOrGetVideo } from 'src/hooks/api/videoQuery';
@@ -23,12 +20,10 @@ interface Props {
 }
 
 const HighlightPlayer = ({ clip }: Props) => {
-  const client = useBoclipsClient();
   const breakpoints = useMediaBreakPoint();
   const isMobileView =
     breakpoints.type === 'mobile' || breakpoints.type === 'tablet';
 
-  const videoLink = client.links.video;
   const { data: video, isLoading: isVideoLoading } = useFindOrGetVideo(
     clip.videoId,
   );
@@ -44,62 +39,58 @@ const HighlightPlayer = ({ clip }: Props) => {
           {formatDuration(clip.clipDuration)}
         </div>
       </div>
-      <VideoPlayer
-        videoLink={
-          new Link({
-            href: expandUrlTemplate(videoLink.href, {
-              id: clip.videoId,
-            }),
-            templated: false,
-          })
-        }
-        segment={{ start: clip.startTime, end: clip.endTime }}
-      />
-      <div className={s.clipDetails}>
-        <h1 className={s.clipTitle}>{clip.clipName}</h1>
-        {!isVideoLoading && <p className={s.channelName}>{video.createdBy}</p>}
-      </div>
-      <div className={s.buttonWrapper}>
-        <div className={s.actionButtons}>
-          <AddToPlaylistButton
-            videoId={clip.videoId}
-            highlightId={clip.clipId}
-            iconOnly={isMobileView}
-            outlineType={false}
-          />
-          {!isVideoLoading && (
-            <FeatureGate product={Product.CLASSROOM}>
-              <VideoShareLinkButton
-                iconOnly={isMobileView}
-                video={video}
-                initialSegment={{ start: clip.startTime, end: clip.endTime }}
-              />
-            </FeatureGate>
-          )}
-          {!isVideoLoading && video?.links?.createEmbedCode && (
-            <EmbedButton
-              video={video}
-              initialSegment={{ start: clip.startTime, end: clip.endTime }}
-              iconOnly={isMobileView}
-              label="Embed"
-              width="auto"
-            />
-          )}
-        </div>
-        <Feedback clipId={clip.clipId} />
-      </div>
       {!isVideoLoading && (
-        <div className={s.videoDetails}>
-          <p>Highlight taken from:</p>
-          <a
-            href={`/videos/${video.id}`}
-            target="new"
-            rel="noopener noreferrer"
-          >
-            <Thumbnail video={video} className={s.thumbnail} />
-            {video.title}
-            <Open />
-          </a>
+        <div>
+          <VideoPlayer
+            video={video}
+            segment={{ start: clip.startTime, end: clip.endTime }}
+          />
+          <div className={s.clipDetails}>
+            <h1 className={s.clipTitle}>{clip.clipName}</h1>
+            <p className={s.channelName}>{video.createdBy}</p>
+          </div>
+          <div className={s.buttonWrapper}>
+            <div className={s.actionButtons}>
+              <AddToPlaylistButton
+                videoId={clip.videoId}
+                highlightId={clip.clipId}
+                iconOnly={isMobileView}
+                outlineType={false}
+              />
+              <FeatureGate product={Product.CLASSROOM}>
+                <VideoShareLinkButton
+                  iconOnly={isMobileView}
+                  video={video}
+                  initialSegment={{
+                    start: clip.startTime,
+                    end: clip.endTime,
+                  }}
+                />
+              </FeatureGate>
+              {video?.links?.createEmbedCode && (
+                <EmbedButton
+                  video={video}
+                  initialSegment={{ start: clip.startTime, end: clip.endTime }}
+                  iconOnly={isMobileView}
+                  label="Embed"
+                  width="auto"
+                />
+              )}
+            </div>
+            <Feedback clipId={clip.clipId} />
+          </div>
+          <div className={s.videoDetails}>
+            <p>Highlight taken from:</p>
+            <a
+              href={`/videos/${video.id}`}
+              target="new"
+              rel="noopener noreferrer"
+            >
+              <Thumbnail video={video} className={s.thumbnail} />
+              {video.title}
+              <Open />
+            </a>
+          </div>
         </div>
       )}
     </div>
