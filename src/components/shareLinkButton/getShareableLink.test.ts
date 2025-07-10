44a0073a7339ea4b2917fc7cd53873016e307462
Ed Cook <edward@boclips.com>
2025-07-10 13:09:00 +0100

    Replace query-string with URL and URLSearchParams

--- src/components/shareLinkButton/getShareableLink.test.ts
+++ src/components/shareLinkButton/getShareableLink.test.ts
@@ -1,4 +1,3 @@
-import { parseUrl } from 'query-string';
 import {
   getShareablePlaylistLink,
   getShareableVideoLink,
@@ -8,35 +7,35 @@ describe('getShareableVideoLink', () => {
   it('creates a link with referer and share indicator', () => {
     const link = getShareableVideoLink('123', 'user-1');
 
-    const parsedUrl = parseUrl(link);
-    expect(parsedUrl.url).toContain('videos/shared/123');
-    expect(parsedUrl.query.referer).toEqual('user-1');
-    expect(parsedUrl.query.segmentStart).toBeUndefined();
-    expect(parsedUrl.query.segmentEnd).toBeUndefined();
+    const parsedUrl = new URL(link);
+    expect(parsedUrl.href).toContain('videos/shared/123');
+    expect(parsedUrl.searchParams.get('referer')).toEqual('user-1');
+    expect(parsedUrl.searchParams.get('segmentStart')).toBeNull();
+    expect(parsedUrl.searchParams.get('segmentEnd')).toBeNull();
   });
 
   it('adds segment bounds when segment is not null', () => {
     const link = getShareableVideoLink('123', 'dave', 10, 20);
 
-    const parsedUrl = parseUrl(link);
-    expect(parsedUrl.query.segmentStart).toEqual('10');
-    expect(parsedUrl.query.segmentEnd).toEqual('20');
+    const parsedUrl = new URL(link);
+    expect(parsedUrl.searchParams.get('segmentStart')).toEqual('10');
+    expect(parsedUrl.searchParams.get('segmentEnd')).toEqual('20');
   });
 
   it('adds segment bounds when segment start is null', () => {
     const link = getShareableVideoLink('123', 'dave', null, 20);
 
-    const parsedUrl = parseUrl(link);
-    expect(parsedUrl.query.segmentStart).toBeUndefined();
-    expect(parsedUrl.query.segmentEnd).toEqual('20');
+    const parsedUrl = new URL(link);
+    expect(parsedUrl.searchParams.get('segmentStart')).toBeNull();
+    expect(parsedUrl.searchParams.get('segmentEnd')).toEqual('20');
   });
 
   it('adds segment bounds when segment end is null', () => {
     const link = getShareableVideoLink('123', 'dave', 10, null);
 
-    const parsedUrl = parseUrl(link);
-    expect(parsedUrl.query.segmentStart).toEqual('10');
-    expect(parsedUrl.query.segmentEnd).toBeUndefined();
+    const parsedUrl = new URL(link);
+    expect(parsedUrl.searchParams.get('segmentStart')).toEqual('10');
+    expect(parsedUrl.searchParams.get('segmentEnd')).toBeNull();
   });
 });
 
@@ -44,8 +43,8 @@ describe('getShareablePlaylistLink', () => {
   it('creates a link with referer and share indicator', () => {
     const link = getShareablePlaylistLink('123', 'user-1');
 
-    const parsedUrl = parseUrl(link);
-    expect(parsedUrl.url).toContain('playlists/shared/123');
-    expect(parsedUrl.query.referer).toEqual('user-1');
+    const parsedUrl = new URL(link);
+    expect(parsedUrl.href).toContain('playlists/shared/123');
+    expect(parsedUrl.searchParams.get('referer')).toEqual('user-1');
   });
 });
