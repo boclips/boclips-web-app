c370991d3a49c17ae316281b4f8f522a576e08b3
Ed Cook <edward@boclips.com>
2026-05-22 12:48:21 +0100

    Batch fetch order item videos in OrderVideoList
    [sc-2753]

--- src/components/orderPage/OrderVideoList.tsx
+++ src/components/orderPage/OrderVideoList.tsx
@@ -1,16 +1,25 @@
 import { OrderItemCard } from 'src/components/orderPage/OrderItemCard';
 import React from 'react';
 import { Order } from 'boclips-api-client/dist/sub-clients/orders/model/Order';
+import { useGetVideos } from 'src/hooks/api/videoQuery';
 
 interface Props {
   order: Order;
 }
 
 export const OrderVideoList = ({ order }: Props) => {
+  const { data: videos } = useGetVideos(
+    order?.items.map((item) => item.video.id) ?? [],
+  );
+
   return (
     <main tabIndex={-1} className="col-start-2 col-end-26">
       {order?.items.map((orderItem) => (
-        <OrderItemCard item={orderItem} key={`id-${orderItem.id}`} />
+        <OrderItemCard
+          item={orderItem}
+          key={`id-${orderItem.id}`}
+          video={videos?.find((video) => video.id === orderItem.video.id)}
+        />
       ))}
     </main>
   );
